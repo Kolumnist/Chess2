25c8a78f87a938eef91e0b46185a56518b4413d5
marku <mbusch@stud.hs-heilbronn.de>
2022-10-10 19:10:41 +0200

    Seed?

--- apis/src/main/java/de/hhn/it/devtools/apis/text_based_labyrinth/Scenario.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/text_based_labyrinth/Scenario.java
@@ -5,5 +5,7 @@ package de.hhn.it.devtools.apis.text_based_labyrinth;
  * The scenario is based on a seed.
  * It defines the layout.
  */
-public class Scenario {
+public interface Scenario {
+
+
 }
---
+++ apis/src/main/java/de/hhn/it/devtools/apis/text_based_labyrinth/Seed.java
@@ -0,0 +1,18 @@
+package de.hhn.it.devtools.apis.text_based_labyrinth;
+
+/**
+ * This is a unique number String.
+ */
+public class Seed {
+
+    private final int Seed;
+
+
+    public Seed(int seedNumber) {
+        this.Seed = seedNumber;
+    }
+
+    public int getSeed() {
+        return Seed;
+    }
+}
