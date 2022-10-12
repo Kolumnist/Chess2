542451bf80a6341724a431b129decbc1f5da51e4
Nedim <nlimani@Stud.hs-heilbronn.de>
2022-10-12 17:23:16 +0200

    Added methods for rotation

--- apis/src/main/java/de/hhn/it/devtools/apis/battleship/BattleshipService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/battleship/BattleshipService.java
@@ -9,7 +9,6 @@ package de.hhn.it.devtools.apis.battleship;
 public interface BattleshipService {
     /**
      * check if possible to place ship at given location.
-     *
      * @param shipType type of ship to determine size
      * @param x1 x start coordinate of the ship
      * @param y1 y start coordinate of the ship
@@ -20,7 +19,7 @@ public interface BattleshipService {
     boolean checkShipPlacingLocation(Ship shipType,int x1, int y1, int x2, int y2);
 
     /**
-     * check if possible to place ship at given location.
+     * places ship at given location
      * @param shipToPlace ship object
      * @param x1 x start coordinate of the ship
      * @param y1 y start coordinate of the ship
@@ -28,6 +27,37 @@ public interface BattleshipService {
      * @param y2 y end coordinate of the ship
      */
     void placeShip(Ship shipToPlace,int x1, int y1, int x2, int y2);
+
+    /**
+     * check if rotation of players ship is possible
+     * @param player player that wants to rotate his ship
+     * @param shipType ship object
+     * @param cx1 current x start coordinate of the ship
+     * @param cy1 current y start coordinate of the ship
+     * @param cx2 current x end coordinate of the ship
+     * @param cy2 current x end coordinate of the ship
+     * @param nx1 new x start coordinate of the ship
+     * @param ny1 new y start coordinate of the ship
+     * @param nx2 new x end coordinate of the ship
+     * @param ny2 new y end coordinate of the ship
+     * @return rotating (not) successful
+     */
+    boolean checkShipRotation(Player player, Ship shipType, int cx1, int cy1, int cx2, int cy2, int nx1, int ny1, int nx2, int ny2);
+
+    /**
+     * rotates the ship with the given coordinates
+     * @param player owner of the ship to be rotated
+     * @param shipToRoate ship object to be rotated
+     * @param cx1 current x start coordinate of the ship
+     * @param cy1 current y start coordinate of the ship
+     * @param cx2 current x end coordinate of the ship
+     * @param cy2 current x end coordinate of the ship
+     * @param nx1 new x start coordinate of the ship
+     * @param ny1 new y start coordinate of the ship
+     * @param nx2 new x end coordinate of the ship
+     * @param ny2 new y end coordinate of the ship
+     */
+    void rotateShip(Player player, Ship shipToRoate, int cx1, int cy1, int cx2, int cy2, int nx1, int ny1, int nx2, int ny2);
 
     /**
      *
@@ -51,7 +81,7 @@ public interface BattleshipService {
     void setGameMode(GameMode chosenGameMode);
 
     /**
-     * adds the type and amount of ships to each players inventory chosen in the game creation menu.
+     * adds the type and amount of ships to each player inventory chosen in the game creation menu.
      * @param typeOfShip type of ship that will be added
      * @param numbeOfShips amount of ships of the certain type that will be added
      */
