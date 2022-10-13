764fb5133a518a843fbf6b4fb0166c524841232d
Nedim <nlimani@Stud.hs-heilbronn.de>
2022-10-13 13:10:00 +0200

    Edited rotation method

--- apis/src/main/java/de/hhn/it/devtools/apis/battleship/BattleshipService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/battleship/BattleshipService.java
@@ -29,35 +29,9 @@ public interface BattleshipService {
     void placeShip(Ship shipToPlace,int x1, int y1, int x2, int y2);
 
     /**
-     * check if rotation of players ship is possible
-     * @param player player that wants to rotate his ship
-     * @param shipType ship object
-     * @param cx1 current x start coordinate of the ship
-     * @param cy1 current y start coordinate of the ship
-     * @param cx2 current x end coordinate of the ship
-     * @param cy2 current x end coordinate of the ship
-     * @param nx1 new x start coordinate of the ship
-     * @param ny1 new y start coordinate of the ship
-     * @param nx2 new x end coordinate of the ship
-     * @param ny2 new y end coordinate of the ship
-     * @return rotating (not) successful
+     * rotates the ship to left or right
      */
-    boolean checkShipRotation(Player player, Ship shipType, int cx1, int cy1, int cx2, int cy2, int nx1, int ny1, int nx2, int ny2);
-
-    /**
-     * rotates the ship with the given coordinates
-     * @param player owner of the ship to be rotated
-     * @param shipToRoate ship object to be rotated
-     * @param cx1 current x start coordinate of the ship
-     * @param cy1 current y start coordinate of the ship
-     * @param cx2 current x end coordinate of the ship
-     * @param cy2 current x end coordinate of the ship
-     * @param nx1 new x start coordinate of the ship
-     * @param ny1 new y start coordinate of the ship
-     * @param nx2 new x end coordinate of the ship
-     * @param ny2 new y end coordinate of the ship
-     */
-    void rotateShip(Player player, Ship shipToRoate, int cx1, int cy1, int cx2, int cy2, int nx1, int ny1, int nx2, int ny2);
+    void rotateShip();
 
     /**
      *
