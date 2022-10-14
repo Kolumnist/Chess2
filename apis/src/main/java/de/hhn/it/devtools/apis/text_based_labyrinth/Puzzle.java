54c93287a3734ce2545f623d12d06b0c2acc8c47
marku <mbusch@stud.hs-heilbronn.de>
2022-10-14 14:31:46 +0200

    Puzzle basic frame.

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/text_based_labyrinth/Puzzle.java
@@ -0,0 +1,50 @@
+package de.hhn.it.devtools.apis.text_based_labyrinth;
+
+public class Puzzle {
+
+
+    private final int puzzleId;
+    private boolean isSolved;
+    private boolean isBeingWorkedOn;
+
+
+    public Puzzle(int id) {
+        puzzleId = id;
+        isSolved = false;
+        isBeingWorkedOn = false;
+    }
+
+
+    public boolean checkIfSolved() {
+        return isSolved;
+    }
+
+    public Puzzle solvePuzzle() {
+        return this;
+    }
+
+    public void execute() {
+        isBeingWorkedOn = true;
+        boolean correctSolutionGiven = false;
+
+        //
+        //Code for execution.
+        //
+
+        //Wow, we found the correct solution!
+        correctSolutionGiven = true;
+
+        if (correctSolutionGiven) {
+            isBeingWorkedOn = false;
+            isSolved = true;
+        }
+    }
+
+    public void setSolved(boolean solution) {
+        isSolved = solution;
+    }
+
+    public int getPuzzleId() {
+        return puzzleId;
+    }
+}
--- apis/src/main/java/de/hhn/it/devtools/apis/text_based_labyrinth/Scenario.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/text_based_labyrinth/Scenario.java
@@ -10,4 +10,5 @@ public interface Scenario {
 
     public void createLayout();
 
+
 }
