a19cf5c2025a4d8398f1655756cd5ea852f17100
Collin <choss@stud.hs-heilbronn.de>
2022-10-14 12:57:50 +0200

    Add Class: Player.java Add Methods to Player.java: get/set for boolean variable Add Method giveUp and ask about wether that is good or not

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/Player.java
@@ -0,0 +1,39 @@
+package de.hhn.it.devtools.apis.chess2;
+
+/** //Interface??//
+ * This class handles all communication between player and components
+ *
+ * @author Collin, Lara, Michel
+ * @version 0.1
+ */
+public class Player {
+
+    boolean lostPiece = false;
+
+    /**
+     * Returns boolean of player losing a piece, on the opponent players turn
+     *
+     * @return boolean of player losing a piece, on the opponent players turn
+     */
+    public boolean isLostPiece() {
+        return lostPiece;
+    }
+
+    /**
+     * Sets if the player lost a piece or not, on the opponent players turn
+     *
+     * @param lostPiece player lost a piece or not, on the opponent players turn
+     */
+    public void setLostPiece(boolean lostPiece) {
+        this.lostPiece = lostPiece;
+    }
+
+    /**
+     * Nachfragen ja nein vll I dont know
+     */
+    void giveUp()
+    {
+
+    }
+
+}
