325b79be30f23d65483acddd3d4a913f14d5d316
Collin <choss@stud.hs-heilbronn.de>
2022-10-14 15:10:05 +0200

    add of standard classes for the facade

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/Handler.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/Handler.java
@@ -1,4 +1,27 @@
 package de.hhn.it.devtools.apis.chess2;
 
 public interface Handler {
+
+    /**
+     *
+     */
+    void startGame();
+
+    /**
+     *
+     */
+    //void buildBoard();
+
+    /**
+     *
+     */
+    Piece getPiece();
+
+    /**
+     *
+     */
+    void setPiece(Field field);
+
+
+
 }
