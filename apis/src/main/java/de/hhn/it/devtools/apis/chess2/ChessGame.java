364ce918d1488ce65304a68e7b1fced74d15e38b
Lara Mangi <lmangi@stud.hs-heilbronn.de>
2022-10-17 16:04:35 +0200

    add new Methods like getCurrentPlayer(), switchCurrentPlayer();... and write comments for these methods

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/ChessGame.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/ChessGame.java
@@ -12,20 +12,78 @@ public class ChessGame {
     private Player player1;
     private Player player2;
 
+    private Player currentPlayer;
+    private Field fieldStatus;
+    private Field currentFieldPos;
+    private Piece currentPiece;
+
+
     protected boolean running = false;
     protected boolean startingPlayer = true;
+
+    /**
+     * Returns the player who's at the beginning of the game on turn
+     *
+     * @return the player who's at the beginning of the game on turn
+     */
+    public boolean getStartingPlayer(){
+        return this.startingPlayer;
+    }
 
     /**
      * Returns the player who's on turn
      *
      * @return the player who's on turn
      */
-    public boolean getStartingPlayer(){
-        return this.startingPlayer;
+    public Player getCurrentPlayer() {
+        return currentPlayer;
     }
 
+    /**
+     * Returns the player who's turn is next
+     *
+     * @return the player who's turn is next
+     */
+    public void switchCurrentPlayer(Player player1, Player player2){
 
-    /*
+    }
+
+    /**
+     * Returns the status of a field who's clicked on (status = example: is this field free?)
+     *
+     * @return the status of a field who's clicked on
+     */
+    public Field getFieldStatus() {
+        return fieldStatus;
+    }
+
+    /**
+     * set the new status of the field
+     *
+     * @set the new status of the field
+     */
+    public void setFieldStatus(Field fieldStatus) {
+        this.fieldStatus = fieldStatus;
+    }
+
+    /**
+     * Returns the field who's clicked on
+     *
+     * @return the field who's clicked on
+     */
+    public Field getCurrentFieldPos() {
+        return currentFieldPos;
+    }
+
+    public void setCurrentFieldPos(int x, int y) {
+        this.currentFieldPos = currentFieldPos;
+    }
+
+    public void setCurrentPiece(int x, int y) {
+        this.currentPiece = currentPiece;
+    }
+
+/*
     startGame() - MainMenu
     openRules() - MainMenu
     closeRules() - MainMenu
---
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/Field.java
@@ -0,0 +1,11 @@
+package de.hhn.it.devtools.apis.chess2;
+
+/**
+ * This class give all the information about the field
+ *
+ * @author Collin, Lara, Michel
+ * @version 0.1
+ */
+
+public class Field {
+}
