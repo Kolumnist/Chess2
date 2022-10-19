5ff81d1fe90ca383df963d35200f824bc95ca8c0
Tristan Nußbaum <gragonator02@gmail.com>
2022-10-19 18:11:40 +0200

    Filled DiceDescriptor.java with fields, constructor and getter and setter. MISSING: comments

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DiceDescriptor.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DiceDescriptor.java
@@ -1,4 +1,27 @@
 package de.hhn.it.devtools.apis.ttrpgsheets;
 
 public class DiceDescriptor {
+    private int size;
+    private int result;
+
+    public DiceDescriptor(int size, int result) {
+        this.size = size;
+        this.result = result;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
 }
