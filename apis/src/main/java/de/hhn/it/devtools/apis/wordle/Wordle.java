8a5e740dbddc20ff589bf7526a60d55ede06f426
MDrautz <madrautz@stud.hs-heilbronn.de>
2022-10-19 15:57:38 +0200

    added javadoc for interface and methods

--- apis/src/main/java/de/hhn/it/devtools/apis/wordle/Wordle.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/wordle/Wordle.java
@@ -1,25 +1,58 @@
 package de.hhn.it.devtools.apis.wordle;
 
+/**
+ * An interface that provides methods for our Wordle micro-project.
+ */
+
 public interface Wordle {
 
+  /**
+   * Method to start the game, i.e. run constructors and show UI.
+   */
   void startGame();
 
+  /**
+   * Method that randomly selects a Wordle solution that the user must attempt to guess.
+   *
+   * @return The randomly selected Wordle solution
+   */
   String selectWordle();
 
+  /**
+   * A method to ensure that the user's guess is 5 letters long.
+   *
+   * @return true if the guess is 5 letters long and false if it is too short
+   */
   boolean validateWordleGuess();
 
+  /**
+   * Compares the user's guess to the solution.
+   */
   void compareGuessToWorldeSolution();
 
+  /**
+   * Updates the UI in accordance with the Wordle rules.
+   */
   void updateUiWithGuess();
 
+  /**
+   * Opens a popup window to notify user of his win.
+   */
   void showVictoryScreen();
 
+  /**
+   * Opens after 6 guesses to notify the player of his loss.
+   */
   void showDefeatScreen();
 
+  /**
+   * Opens a popup window that shows the rules to Wordle.
+   */
   void showRules();
 
+  /**
+   * Quits the game and closes all Wordle windows.
+   */
   void quitGame();
-
-
 
 }
