ceb164399eb0db58a0ec53f786522646fa9065a2
Tristan Nußbaum <gragonator02@gmail.com>
2022-10-19 18:24:29 +0200

    Filled StatDescriptor.java with fields, constructor and getter and setter Missing Comments

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/StatDescriptor.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/StatDescriptor.java
@@ -1,4 +1,27 @@
 package de.hhn.it.devtools.apis.ttrpgsheets;
 
 public class StatDescriptor {
+    private StatType statType;
+    private int value;
+
+    public StatDescriptor(StatType statType, int value) {
+        this.statType = statType;
+        this.value = value;
+    }
+
+    public StatType getStatType() {
+        return statType;
+    }
+
+    public void setStatType(StatType statType) {
+        this.statType = statType;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
