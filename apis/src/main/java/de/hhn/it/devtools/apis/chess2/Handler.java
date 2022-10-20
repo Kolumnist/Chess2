ba7e6c6467a00802f44690f0b395fecc92eb31ab
Michel Jouaux <mjouaux@stud.hs-heilbronn.de>
2022-10-20 21:07:34 +0200

    Add Methods: giveUp(), activatePieceButton(ArrayList<int[]> piecePos), activateFreeButton(), setPiecePosition(int[] pos), FieldState getFieldState()

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/Handler.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/Handler.java
@@ -1,5 +1,7 @@
 package de.hhn.it.devtools.apis.chess2;
 
+import java.util.ArrayList;
+
 /**
  * This class handles all communication between player and components
  *
@@ -12,13 +14,18 @@ public interface Handler {
      * build board with all fields and the 4 jail fields, place pieces on the board,
      * set the starting player and inform the players about who starts.
      */
-    void startGame();
+    void startNewGame();
 
     /**
      * resets the board and pieces and the kings bananas. Assigns the pieces to their players
      * and sets the starting player, informs the players about who starts
      */
     void reset();
+
+    /**
+     *
+     */
+    void giveUp();
 
     /**
      * displays the rules as text
@@ -28,6 +35,16 @@ public interface Handler {
     void openRules();
 
     /**
+     *
+     */
+    void activatePieceButton(ArrayList<int[]> piecePos);
+
+    /**
+     *
+     */
+    void activateFreeButton();
+
+    /**
      * The king/queen gets send to jail upon defeat, the jail field is chosen
      * by the player who defeated the piece
      *
@@ -35,4 +52,15 @@ public interface Handler {
      * @param jail for 0-3
      */
     void setPieceInJail(Piece piece, int[] jail);
+
+    /**
+     *
+     */
+    void setPiecePosition(int[] pos);
+
+    /**
+     *
+     * @return
+     */
+    FieldState getFieldState();
 }
