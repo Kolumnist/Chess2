bf48774eb47b9541edf19f0125ccb365f398ee8c
Leo <leonidas01032002@gmail.com>
2022-10-20 23:58:08 +0200

    Demo programm erstellt

---
+++ apis/src/test/java/CandyCrush/DemoCandyCrushUsage.java
@@ -0,0 +1,13 @@
+package CandyCrush;
+
+import de.hhn.it.devtools.apis.candycrush.CandyCrushGame;
+import de.hhn.it.devtools.apis.candycrush.CandyCrushInterface;
+
+public class DemoCandyCrushUsage {
+    public static void main(String[] args){
+        CandyCrushInterface candycrush= null;
+        candycrush.startNewGame();
+    }
+
+
+}
