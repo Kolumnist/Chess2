407f72477b2c9d17cf0e687006f9d73ff89b915e
Collin <choss@stud.hs-heilbronn.de>
2022-10-20 21:54:01 +0200

    javadoc for FieldState.java all states

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/FieldState.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/FieldState.java
@@ -1,7 +1,7 @@
 package de.hhn.it.devtools.apis.chess2;
 
 /**
- * here are all states a Field can have
+ * Enum to describe the different fields and if there is something on it or not
  *
  * @author Collin, Lara, Michel
  * @version 0.1.1
@@ -9,8 +9,15 @@ package de.hhn.it.devtools.apis.chess2;
 
 public enum FieldState {
 
-    JAIL_KING, JAIL_QUEEN, //JAIL, we dont really need it if we make the jail a possible move
-    HAS_CURRENT_PIECE, HAS_OTHER_PIECE,
+    /** Is a Jail Field and has a King on it */
+    JAIL_KING,
+    /** Is a Jail Field and has a Queen on it */
+    JAIL_QUEEN, //JAIL, we dont really need it if we make the jail a possible move
+    /** (NO JAIL) A Piece of the current Player is on the Field */
+    HAS_CURRENT_PIECE,
+    /** (NO JAIL) Any other Piece is on the Field */
+    HAS_OTHER_PIECE,
+    /** Any other Field */
     FREE_FIELD
 
 }
