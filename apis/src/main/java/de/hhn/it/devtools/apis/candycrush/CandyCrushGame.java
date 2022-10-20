7964d377590b9b351a579ee3bf5e6582b77f4780
Leo <leonidas01032002@gmail.com>
2022-10-20 21:58:10 +0200

    Klassen hinzugefügt, zeug reingeschrieben

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/candycrush/CandyCrushGame.java
@@ -0,0 +1,33 @@
+package de.hhn.it.devtools.apis.candycrush;
+/**
+ * This class give all the information the player needs to know
+ *
+ * @author Leonidas Zissis, Alexander Schwab, Elias Haag
+ * @version 1.0
+ */
+
+public class CandyCrushGame {
+    private Profile profile1; // oder einfach nur profile und profile2und3 und current gibts ned
+    private Profile profile2;
+    private Profile profile3;
+    private Profile currentProfile;
+
+    private Field field;
+    private Block block;
+
+    protected Boolean running = false;
+    protected String GameMode ="time";  // eventuel boolean oder integer oder so? wie bei minecraft gamemode 0,1,2
+    /**
+     * Returns the Gamemode
+     *
+     * @return the Gamemode
+     */
+
+
+
+
+
+
+
+
+}
--- apis/src/main/java/de/hhn/it/devtools/apis/candycrush/GameMode.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/candycrush/GameMode.java
@@ -1,4 +1,8 @@
 package de.hhn.it.devtools.apis.candycrush;
 
 public class GameMode {
+    private GameMode gamemode;
+    public GameMode getGameMode(){
+        return this.gamemode;
+    }
 }
