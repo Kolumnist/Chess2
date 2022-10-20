193cc9eeeb84a1221e81539637b45714a5520b91
Collin <choss@stud.hs-heilbronn.de>
2022-10-20 21:57:36 +0200

    version change in FieldState.java from 0.1.1 to 1.0

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/FieldState.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/FieldState.java
@@ -4,7 +4,7 @@ package de.hhn.it.devtools.apis.chess2;
  * Enum to describe the different fields and if there is something on it or not
  *
  * @author Collin, Lara, Michel
- * @version 0.1.1
+ * @version 1.0
  */
 
 public enum FieldState {
@@ -12,7 +12,10 @@ public enum FieldState {
     /** Is a Jail Field and has a King on it */
     JAIL_KING,
     /** Is a Jail Field and has a Queen on it */
-    JAIL_QUEEN, //JAIL, we dont really need it if we make the jail a possible move
+    JAIL_QUEEN,
+
+    /* JAIL, we don't really need it if we make the jail a possible move */
+
     /** (NO JAIL) A Piece of the current Player is on the Field */
     HAS_CURRENT_PIECE,
     /** (NO JAIL) Any other Piece is on the Field */
