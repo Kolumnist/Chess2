48bac0c3627fc22a3a9877ff58ecadfd99a74dbb
Leo <leonidas01032002@gmail.com>
2022-10-20 23:01:17 +0200

    GameMode angepasst

--- apis/src/main/java/de/hhn/it/devtools/apis/candycrush/GameMode.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/candycrush/GameMode.java
@@ -1,8 +1,7 @@
 package de.hhn.it.devtools.apis.candycrush;
 
-public class GameMode {
-    private GameMode gamemode;
-    public GameMode getGameMode(){
-        return this.gamemode;
-    }
+public enum GameMode {
+    // The GameMode is against the Time
+    TIME
+
 }
