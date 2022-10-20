9a00d792001e1132765fc1913b11bf4ddee678ea
Leo <leonidas01032002@gmail.com>
2022-10-20 23:40:16 +0200

    Interface angepasst

--- apis/src/main/java/de/hhn/it/devtools/apis/candycrush/CandyCrushInterface.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/candycrush/CandyCrushInterface.java
@@ -1,4 +1,23 @@
 package de.hhn.it.devtools.apis.candycrush;
 
 public interface CandyCrushInterface {
+    /**
+     * build field with all Blocks, place Blocks on the field, use selected GameMode
+     */
+    void startNewGame();
+
+    /**
+     * pauses the game (freeze time & no moves allowed)
+     */
+    void pause();
+
+    /**
+     * creates a window and shows the Highscors of the profiles
+     */
+    void showStats();
+
+    /**
+     * creates a window and displays the rules as text
+     */
+    void openRules();
 }
