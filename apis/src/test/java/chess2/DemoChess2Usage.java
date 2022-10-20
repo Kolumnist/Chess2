25c87fddc709309ec940f35c95be262f0499911f
Lara Mangi <lmangi@stud.hs-heilbronn.de>
2022-10-20 22:47:39 +0200

    Create new Package and class for a test

---
+++ apis/src/test/java/chess2/DemoChess2Usage.java
@@ -0,0 +1,11 @@
+package chess2;
+
+import de.hhn.it.devtools.apis.chess2.Chess2Service;
+
+public class DemoChess2Usage {
+
+    public static void main(String[] args) {
+        Chess2Service chess2 = null;
+        chess2.startNewGame();
+    }
+}
