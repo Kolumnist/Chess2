ffb21b90301843941a14b013e8d24e2a9fc4e864
Collin <choss@stud.hs-heilbronn.de>
2022-10-20 21:23:49 +0200

    Much more thinking has happened
    delete classes: ChessGame.java, Field.java and Player.java
    change FieldState.java - to version 0.1.1

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/FieldState.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/FieldState.java
@@ -4,16 +4,13 @@ package de.hhn.it.devtools.apis.chess2;
  * here are all states a Field can have
  *
  * @author Collin, Lara, Michel
- * @version 0.1
+ * @version 0.1.1
  */
 
 public enum FieldState {
 
-    FREE_JAIL, QUEEN_FOE_OCCUPIED_JAIL, KING_FOE_OCCUPIED_JAIL,
-    QUEEN_PAL_OCCUPIED_JAIL, KING_PAL_OCCUPIED_JAIL,
-
-    //with GRAY I mean not RED(any other colors are fine too)
-    FREE_GRAY, FREE_RED, FOE_OCCUPIED_GRAY, FOE_OCCUPIED_RED,
-    PAL_OCCUPIED_GRAY, PAL_OCCUPIED_RED,
+    JAIL_KING, JAIL_QUEEN, //JAIL, we dont really need it if we make the jail a possible move
+    HAS_CURRENT_PIECE, HAS_OTHER_PIECE,
+    FREE_FIELD
 
 }
--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/Piece.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/Piece.java
@@ -1,40 +1,11 @@
 package de.hhn.it.devtools.apis.chess2;
 
-import java.util.ArrayList;
-
 /**
- * This class handles all communication between piece and components
+ * This interface does nothing
  *
  * @author Collin, Lara, Michel
  * @version 0.1
  */
-public abstract class Piece {
-    private int [] position;
-    private boolean defeated;
 
-    /**
-     * Sets the position of the piece if it was moved.
-     *
-     * @param position the position of the piece on the board.
-     */
-    public void setPosition(int[] position){this.position = position;}
-
-    /**
-     * Returns the position of the piece on the board as an int[].
-     *
-     * @return int[] of the position of the piece on the board.
-     */
-    public int[] getPosition(){return position;}
-
-    /**
-     * Returns all the possible positions the piece can have.
-     *
-     * @return ArrayList of all the possible positions the piece can go to.
-     */
-    public abstract ArrayList<int[]> showPossibleMove();
-
-    /**
-     * Not sure if this methode is needed.
-     */
-    //protected abstract void move(int[] position);
+public interface Piece {
 }
