d70504853d204b400666cf87a79000e4c46ed7c1
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2022-10-21 14:48:07 +0200

    Formatted the DiceType Enum to fit the Google Checkstyle

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DiceType.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DiceType.java
@@ -1,9 +1,9 @@
 package de.hhn.it.devtools.apis.ttrpgsheets;
 
 /**
- * Enum to represent the Dice Types
- * Dice names start with a D for "Dice" followed by a number which stands for the maximum value
- * The most common Die is the D6 for example
+ * Enum to represent the Dice Types.
+ * Dice names start with a D for "Dice" followed by a number which stands for the maximum value.
+ * The most common Die is the D6 for example.
  */
 public enum DiceType {
     D2,
