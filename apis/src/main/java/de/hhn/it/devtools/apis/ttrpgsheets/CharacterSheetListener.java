ee8820803acc55b1667658131c1388693ea3a3c9
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2022-10-21 14:19:24 +0200

    Formatted the CharacterSheetListener Interface to fit the Google Checkstyle

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/CharacterSheetListener.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/CharacterSheetListener.java
@@ -5,24 +5,24 @@ package de.hhn.it.devtools.apis.ttrpgsheets;
  */
 public interface CharacterSheetListener {
 
-    /**
-     * Informs the listener that the stats of the character sheet changed.
-     *
-     * @param stat actual stat of the character sheet
-     */
-    void statChanged(StatDescriptor stat);
+  /**
+   * Informs the listener that the stats of the character sheet changed.
+   *
+   * @param stat actual stat of the character sheet
+   */
+  void statChanged(StatDescriptor stat);
 
-    /**
-     * Informs the listener that the dice of the character sheet changed.
-     *
-     * @param dice actual value of the dice
-     */
-    void diceChanged(DiceDescriptor dice);
+  /**
+   * Informs the listener that the dice of the character sheet changed.
+   *
+   * @param dice actual value of the dice
+   */
+  void diceChanged(DiceDescriptor dice);
 
-    /**
-     * Informs the listener that the description of the character sheet changed.
-     *
-     * @param description actual value of the description
-     */
-    void descriptionChanged(DescriptionDescriptor description);
+  /**
+   * Informs the listener that the description of the character sheet changed.
+   *
+   * @param description actual value of the description
+   */
+  void descriptionChanged(DescriptionDescriptor description);
 }
