3401ff80f9ff497477edc0cb2881a2ea0de7b932
Patrick Franz <patrick.franz@franz-vs.de>
2022-10-21 15:49:29 +0200

    deleted unnecessary methods after feedback.

--- apis/src/main/java/de/hhn/it/devtools/apis/wordle/WordleService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/wordle/WordleService.java
@@ -24,43 +24,11 @@ public interface WordleService {
   String selectWordle();
 
   /**
-   * For the user to enter a letter for their guess using the WordleKeyListener.
-   */
-  void enterLetter(WordleKeyListener keyListener);
-
-  void deleteLetter(WordleKeyListener keyListener);
-
-  /**
    * A method to ensure that the user's guess is 5 letters long. An exception is thrown if it isn't.
    *
    * @return true if the guess is 5 letters long
    */
   boolean validateWordleGuess() throws IllegalGuessException;
-
-  /**
-   * Compares the user's guess to the solution.
-   */
-  void compareGuessToWordleSolution();
-
-  /**
-   * Updates the UI in accordance with the Wordle rules.
-   */
-  void updateUiWithColours();
-
-  /**
-   * Opens a popup window to notify user of his win.
-   */
-  void showVictoryScreen();
-
-  /**
-   * Opens after 6 guesses to notify the player of his loss.
-   */
-  void showDefeatScreen();
-
-  /**
-   * Opens a popup window that shows the rules to Wordle.
-   */
-  void showRules();
 
   /**
    * Quits the game and closes all Wordle windows.
--- apis/src/test/java/wordle/WordleServiceUsageDemo.java
+++ apis/src/test/java/wordle/WordleServiceUsageDemo.java
@@ -17,47 +17,16 @@ public class WordleServiceUsageDemo {
 
     demo.selectWordle(); // System selects a Wordle for the user to guess
 
-    demo.showRules();
-
-    demo.enterLetter(demoListener); // repeated until five letters are entered
-
     demo.validateWordleGuess(); // System validates the user's guess
-
-    demo.compareGuessToWordleSolution(); // returns false
-
-    demo.updateUiWithColours();
-
-    demo.enterLetter(demoListener); // repeated four times
 
     demo.validateWordleGuess(); // throws IllegalGuessException
 
-    demo.enterLetter(demoListener); // a fifth letter is entered
-
-    demo.deleteLetter(demoListener); // fifth letter is deleted
-
-    demo.enterLetter(demoListener); // another fifth letter is entered
-
     demo.validateWordleGuess();
-
-    demo.compareGuessToWordleSolution(); //returns true
-
-    demo.updateUiWithColours(); // all WordlePanels turn green
-
-    demo.showVictoryScreen();
 
     demo.startAnotherGame();
 
     demo.selectWordle(); // a new Wordle is selected for the user to guess
 
-    demo.enterLetter(demoListener); // repeated five times
-
     demo.validateWordleGuess();
-
-    demo.compareGuessToWordleSolution(); //returns true
-
-    demo.updateUiWithColours(); // all WordlePanels turn green
-
-    demo.showVictoryScreen();
-
   }
 }
