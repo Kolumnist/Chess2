bce40a7fd28ec2ee3e44f286066f7c60f53e5523
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2022-10-21 14:48:38 +0200

    Formatted the StatDescriptor Class to fit the Google Checkstyle

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/StatDescriptor.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/StatDescriptor.java
@@ -1,59 +1,62 @@
 package de.hhn.it.devtools.apis.ttrpgsheets;
 
+/**
+ * The Descriptor for the Stat Class.
+ */
 public class StatDescriptor {
-    private StatType statType;
-    private int value;
+  private StatType statType;
+  private int value;
 
-    /**
-     * Constructor stating {@link StatType} and value of the Stat.
-     *
-     * @param statType the type of the Stat
-     * @param value the value of the Stat
-     */
-    public StatDescriptor(StatType statType, int value) {
-        this.statType = statType;
-        this.value = value;
-    }
+  /**
+   * Constructor stating {@link StatType} and value of the Stat.
+   *
+   * @param statType the type of the Stat
+   * @param value the value of the Stat
+   */
+  public StatDescriptor(StatType statType, int value) {
+    this.statType = statType;
+    this.value = value;
+  }
 
-    /**
-     * Returns the {@link StatType} of the Stat.
-     *
-     * @return {@link StatType} of the Stat
-     */
-    public StatType getStatType() {
-        return statType;
-    }
+  /**
+   * Returns the {@link StatType} of the Stat.
+   *
+   * @return {@link StatType} of the Stat
+   */
+  public StatType getStatType() {
+    return statType;
+  }
 
-    /**
-     * Sets the {@link StatType} of the Stat.
-     *
-     * @param statType type of Stat
-     */
-    public void setStatType(StatType statType) {
-        this.statType = statType;
-    }
+  /**
+   * Sets the {@link StatType} of the Stat.
+   *
+   * @param statType type of Stat
+   */
+  public void setStatType(StatType statType) {
+    this.statType = statType;
+  }
 
-    /**
-     * Returns the value of the Stat.
-     *
-     * @return value of the Stat
-     */
-    public int getValue() {
-        return value;
-    }
+  /**
+   * Returns the value of the Stat.
+   *
+   * @return value of the Stat
+   */
+  public int getValue() {
+    return value;
+  }
 
-    /**
-     * Sets the value of the Stat.
-     *
-     * @param value value of the Stat
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
+  /**
+   * Sets the value of the Stat.
+   *
+   * @param value value of the Stat
+   */
+  public void setValue(int value) {
+    this.value = value;
+  }
 
-    @Override
-    public String toString() {
-        return "Stat type=" + statType +
-                ", Value=" + value;
-    }
+  @Override
+  public String toString() {
+    return "Stat type=" + statType
+            + ", Value=" + value;
+  }
 }
