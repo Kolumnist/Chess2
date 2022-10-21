08f98c33c7ab2fc643e582d1a1804d129c1d403f
Nedim <nlimani@Stud.hs-heilbronn.de>
2022-10-21 15:27:59 +0200

    added ship types and sizes

--- apis/src/main/java/de/hhn/it/devtools/apis/battleship/Ship.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/battleship/Ship.java
@@ -4,5 +4,28 @@ package de.hhn.it.devtools.apis.battleship;
  * creates ship objects.
  */
 
-public class Ship {
+public enum Ship {
+    //types of ships and their size
+    Carrier(5),
+    Battleship(4),
+    Cruiser(3),
+    Submarine(3),
+    Destroyer(2);
+
+
+    //final variable for the ship size
+    private final int size;
+
+
+    //constructor
+    Ship (int size){
+        this.size = size;
+    }
+
+
+    //return the ship size of the respective ship
+    public int getNumber(){
+        return size;
+    }
+
 }
