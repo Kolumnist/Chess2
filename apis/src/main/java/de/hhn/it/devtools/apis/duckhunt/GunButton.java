847821d90697ccf68ab96880c513721e8721e8cd
Hannes Jetter <hannes.jetter@gmail.com>
2022-10-21 17:25:12 +0200

    added enum for mouse buttons

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/GunButton.java
@@ -0,0 +1,13 @@
+package de.hhn.it.devtools.apis.duckhunt;
+
+/**
+ * Enum to represent the different buttons on the mouse.
+ */
+public enum GunButton {
+  /** The left mouse button is pressed. */
+  LEFT,
+  /** The right mouse button is pressed. */
+  RIGHT,
+  /** The middle mouse button is pressed. */
+  MIDDLE
+}
