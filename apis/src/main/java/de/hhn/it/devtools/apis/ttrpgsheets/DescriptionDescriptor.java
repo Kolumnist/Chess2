fb54cc062fda87445a1fdcbe8c61dddfce608ab9
Tristan Nußbaum <gragonator02@gmail.com>
2022-10-21 10:36:54 +0200

    Added comments to DescriptionDescriptor.java, DiceDescriptor.java and StatDescriptor.java

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DescriptionDescriptor.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DescriptionDescriptor.java
@@ -4,23 +4,49 @@ public class DescriptionDescriptor {
     private DescriptionType descriptionType;
     private String text;
 
+    /**
+     * Constructor stating {@link DescriptionType} and text of the Description.
+     *
+     * @param descriptionType the type of the Description
+     * @param text the text within the Description
+     */
     public DescriptionDescriptor(DescriptionType descriptionType, String text) {
         this.descriptionType = descriptionType;
         this.text = text;
     }
 
+    /**
+     * Returns the {@link DescriptionType} of the Description.
+     *
+     * @return {@link DescriptionType} of the Description
+     */
     public DescriptionType getDescriptionType() {
         return descriptionType;
     }
 
+    /**
+     * Sets the {@link DescriptionType} of the Description.
+     *
+     * @param descriptionType type of Description
+     */
     public void setDescriptionType(DescriptionType descriptionType) {
         this.descriptionType = descriptionType;
     }
 
+    /**
+     * Returns the text of the Description.
+     *
+     * @return text of the Description
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Sets the text of the Description.
+     *
+     * @param text of the Description
+     */
     public void setText(String text) {
         this.text = text;
     }
--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DiceDescriptor.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DiceDescriptor.java
@@ -4,23 +4,49 @@ public class DiceDescriptor {
     private DiceType diceType;
     private int result;
 
+    /**
+     * Constructor stating {@link DiceType} and result of the Dice.
+     *
+     * @param diceType the type of the Dice
+     * @param result the result of the last dice throw
+     */
     public DiceDescriptor(DiceType diceType, int result) {
         this.diceType = diceType;
         this.result = result;
     }
 
+    /**
+     * Returns the {@link DiceType} of the Dice.
+     *
+     * @return {@link DiceType} of the Dice
+     */
     public DiceType getDiceType() {
         return diceType;
     }
 
+    /**
+     * Sets the {@link DiceType} of the Dice.
+     *
+     * @param diceType type of Dice
+     */
     public void setDiceType(DiceType diceType) {
         this.diceType = diceType;
     }
 
+    /**
+     * Returns the result of the Dice.
+     *
+     * @return result of the last dice throw
+     */
     public int getResult() {
         return result;
     }
 
+    /**
+     * Sets the result of the dice throw.
+     *
+     * @param result result of the dice throw
+     */
     public void setResult(int result) {
         this.result = result;
     }
