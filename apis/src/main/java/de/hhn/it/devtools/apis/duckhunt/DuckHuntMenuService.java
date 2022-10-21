b96f38cea8e724f92b8d8f88bdd78a374ca3e1f2
Hannes Jetter <hannes.jetter@gmail.com>
2022-10-21 17:15:18 +0200

    added javadoc

--- apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/DuckHuntMenuService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/DuckHuntMenuService.java
@@ -6,5 +6,10 @@ package de.hhn.it.devtools.apis.duckhunt;
 public interface DuckHuntMenuService {
   Integer id = null;
 
+  /**
+   * Is called when mouse button is clicked on a button and provides button id.
+   *
+   * @param buttonId id of the button that was pressed
+   */
   void onButtonClicked(int buttonId);
 }
--- apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/DuckHuntService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/DuckHuntService.java
@@ -4,5 +4,11 @@ package de.hhn.it.devtools.apis.duckhunt;
  * Allows ui interaction.
  */
 public interface DuckHuntService {
+  /**
+   * Is called when mouse button is clicked and provides the x and y cursor position.
+   *
+   * @param x x cursor position
+   * @param y y cursor position
+   */
   void onClicked(int x, int y);
 }
