4ceabe678751bd32c28d568b2f7d542f69cc71c7
marku <mbusch@stud.hs-heilbronn.de>
2022-10-21 10:29:04 +0200

    You do not write user big.

--- apis/src/main/java/de/hhn/it/devtools/apis/textbasedlabyrinth/GameService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/textbasedlabyrinth/GameService.java
@@ -3,37 +3,37 @@ package de.hhn.it.devtools.apis.textbasedlabyrinth;
 import de.hhn.it.devtools.apis.textbasedlabyrinth.exceptions.RoomFailedException;
 
 /**
- * Interface for the Game
+ * Interface for the game.
  */
 public interface GameService {
 
     /**
-     * Let the User move the The room to the South(if possible)
+     * Let the user move the room to the South(if possible)
      */
     void moveSouth() throws RoomFailedException;
 
     /**
-     * Let the User move the The room to the North(if possible)
+     * Let the user move the room to the North(if possible)
      */
     void moveNorth() throws RoomFailedException;
 
     /**
-     * Let the User move the The room to the West(if possible)
+     * Let the user move the room to the West(if possible)
      */
     void moveWest() throws RoomFailedException;
 
     /**
-     * Let the User move the The room to the East(if possible)
+     * Let the user move the room to the East(if possible)
      */
     void moveEast() throws RoomFailedException;
 
     /**
-     * Let the User interact with the environment
+     * Let the user interact with the environment
      */
     void interaction();
 
     /**
-     * Let the User search through the room
+     * Let the user search through the room
      */
     void searchRoom() throws RoomFailedException;
 }
