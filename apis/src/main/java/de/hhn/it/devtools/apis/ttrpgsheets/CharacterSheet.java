027084e6fd10f8902e2264ed716d7bde4e9cb834
Gabriel Schmidlin Kitzig <gschmidlin@stud.hs-heilbronn.de>
2022-10-21 15:15:10 +0200

    Added getMethods and comments of "DiceDescriptor", "DescriptionDescriptor", "StatDescriptor"

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/CharacterSheet.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/CharacterSheet.java
@@ -16,7 +16,7 @@ public interface CharacterSheet {
   /**
    * increments the given {@link StatType} based upon the {@link OriginType}.
    *
-   * @param stat the Type of Stat to increment
+   * @param stat   the Type of Stat to increment
    * @param origin the origin of the change
    */
   void incrementStat(StatType stat, OriginType origin);
@@ -24,7 +24,7 @@ public interface CharacterSheet {
   /**
    * Increments the given {@link StatType} based upon the {@link OriginType} by a given amount.
    *
-   * @param stat the Type of Stat to increment
+   * @param stat   the Type of Stat to increment
    * @param origin the origin of the change
    * @param amount the amount the Stat changes
    */
@@ -33,7 +33,7 @@ public interface CharacterSheet {
   /**
    * Decrements the given {@link StatType} based upon the {@link OriginType}.
    *
-   * @param stat the Type of Stat to decrement
+   * @param stat   the Type of Stat to decrement
    * @param origin the origin of the change
    */
   void decrementStat(StatType stat, OriginType origin);
@@ -41,19 +41,33 @@ public interface CharacterSheet {
   /**
    * Decrements the given {@link StatType} based upon the {@link OriginType} by a given amount.
    *
-   * @param stat the Type of Stat to decrement
+   * @param stat   the Type of Stat to decrement
    * @param origin the origin of the change
    * @param amount the amount the Stat changes
    */
   void decrementStat(StatType stat, OriginType origin, int amount);
 
   /**
+   * Returns descriptor of the stat.
+   *
+   * @return descriptor of the stat
+   */
+  StatDescriptor getStat(StatType stat);
+
+  /**
    * Changes the description of the given {@link DescriptionType} to the String.
    *
    * @param description the type of description to change
-   * @param text the text the description is changed to
+   * @param text        the text the description is changed to
    */
   void changeDescription(DescriptionType description, String text);
+
+  /**
+   * Returns descriptor of description type.
+   *
+   * @return descriptor of description type
+   */
+  DescriptionDescriptor getDescriptionType(DescriptionType description);
 
   /**
    * Rolls the dice to generate a random number according to the current dice.
@@ -66,5 +80,12 @@ public interface CharacterSheet {
    * @param dice the type of dice to be used further
    */
   void changeDiceType(DiceType dice);
+
+  /**
+   * Returns the descriptor of the Dice.
+   *
+   * @return descriptor of the Dice
+   */
+  DiceDescriptor getDice();
 
 }
