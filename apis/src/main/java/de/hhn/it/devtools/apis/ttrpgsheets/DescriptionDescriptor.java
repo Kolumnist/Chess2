e099519c7ec4d1236c1f739f2074414e045c3d26
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2022-10-21 14:22:28 +0200

    Formatted the DescriptionDescriptor Class to fit the Google Checkstyle

--- apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DescriptionDescriptor.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/ttrpgsheets/DescriptionDescriptor.java
@@ -1,59 +1,62 @@
 package de.hhn.it.devtools.apis.ttrpgsheets;
 
+/**
+ * The Descriptor for the Description Class.
+ */
 public class DescriptionDescriptor {
-    private DescriptionType descriptionType;
-    private String text;
+  private DescriptionType descriptionType;
+  private String text;
 
-    /**
-     * Constructor stating {@link DescriptionType} and text of the Description.
-     *
-     * @param descriptionType the type of the Description
-     * @param text the text within the Description
-     */
-    public DescriptionDescriptor(DescriptionType descriptionType, String text) {
-        this.descriptionType = descriptionType;
-        this.text = text;
-    }
+  /**
+   * Constructor stating {@link DescriptionType} and text of the Description.
+   *
+   * @param descriptionType the type of the Description
+   * @param text the text within the Description
+   */
+  public DescriptionDescriptor(DescriptionType descriptionType, String text) {
+    this.descriptionType = descriptionType;
+    this.text = text;
+  }
 
-    /**
-     * Returns the {@link DescriptionType} of the Description.
-     *
-     * @return {@link DescriptionType} of the Description
-     */
-    public DescriptionType getDescriptionType() {
-        return descriptionType;
-    }
+  /**
+   * Returns the {@link DescriptionType} of the Description.
+   *
+   * @return {@link DescriptionType} of the Description
+   */
+  public DescriptionType getDescriptionType() {
+    return descriptionType;
+  }
 
-    /**
-     * Sets the {@link DescriptionType} of the Description.
-     *
-     * @param descriptionType type of Description
-     */
-    public void setDescriptionType(DescriptionType descriptionType) {
-        this.descriptionType = descriptionType;
-    }
+  /**
+   * Sets the {@link DescriptionType} of the Description.
+   *
+   * @param descriptionType type of Description
+   */
+  public void setDescriptionType(DescriptionType descriptionType) {
+    this.descriptionType = descriptionType;
+  }
 
-    /**
-     * Returns the text of the Description.
-     *
-     * @return text of the Description
-     */
-    public String getText() {
-        return text;
-    }
+  /**
+   * Returns the text of the Description.
+   *
+   * @return text of the Description
+   */
+  public String getText() {
+    return text;
+  }
 
-    /**
-     * Sets the text of the Description.
-     *
-     * @param text of the Description
-     */
-    public void setText(String text) {
-        this.text = text;
-    }
+  /**
+   * Sets the text of the Description.
+   *
+   * @param text of the Description
+   */
+  public void setText(String text) {
+    this.text = text;
+  }
 
-    @Override
-    public String toString() {
-        return "Description type = " + descriptionType +
-                ", Text = " + text;
-    }
+  @Override
+  public String toString() {
+    return "Description type = " + descriptionType
+            + ", Text = " + text;
+  }
 }
