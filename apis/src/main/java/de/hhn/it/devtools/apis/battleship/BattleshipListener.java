0db6e999f3adf5293d6d5802e3647d54fe076667
nuri <hendennurullah@gmail.com>
2022-10-27 00:44:15 +0200

    changed minor misspellings

--- apis/src/main/java/de/hhn/it/devtools/apis/battleship/BattleshipListener.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/battleship/BattleshipListener.java
@@ -1,6 +1,5 @@
 package de.hhn.it.devtools.apis.battleship;
 
-import de.hhn.it.devtools.apis.examples.coffeemakerservice.State;
 
 /**
  * Callback to notify observers about a state change of the game.
@@ -10,7 +9,7 @@ public interface BattleshipListener {
 
     /**
      *  Informs the listener that the game has changed its state.
-     * @param state actual state of the CoffeeMaker
+     * @param state current state of the game
      */
     void newState(GameState state);
 }
