a5405bcab56b236e1da3a8671f16ae540f507e6b
Silas-HHN <68948614+Silas-HHN@users.noreply.github.com>
2022-10-27 17:20:53 +0200

    >>Connect Four<< update.

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/connectfour/ProfileNotSelectedException.java
@@ -0,0 +1,8 @@
+package de.hhn.it.devtools.apis.connectfour;
+
+/**
+ * This Class models a profile not selected exception, which might occur when the user tries to
+ * start a game without selecting the profile(s).
+ */
+public class ProfileNotSelectedException extends Throwable {
+}
