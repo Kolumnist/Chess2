e578be57a695c85d708e42bbeb2b26a7b3b561b8
Hannes Jetter <hannes.jetter@gmail.com>
2022-10-27 18:31:38 +0200

    added methods for the menu interface

--- apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/DuckHuntMenuService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/DuckHuntMenuService.java
@@ -1,15 +1,39 @@
 package de.hhn.it.devtools.apis.duckhunt;
 
+import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
+
 /**
  * Allows menu ui interaction.
  */
 public interface DuckHuntMenuService {
-  Integer id = null;
-
   /**
    * Is called when mouse button is clicked on a button and provides button id.
    *
    * @param buttonId id of the button that was pressed
    */
-  void onButtonClicked(int buttonId);
+  void onButtonClicked(int buttonId) throws IllegalButtonId;
+
+  /**
+   * Adds a listener to get updates for the menu.
+   *
+   * @param listener object implementing the listener interface
+   * @throws IllegalParameterException if the listener is a null reference.
+   */
+  void addCallback(DuckHuntListener listener) throws IllegalParameterException;
+
+  /**
+   * Removes a listener.
+   *
+   * @param listener object implementing the listener interface
+   * @throws IllegalParameterException if the listener is a null reference.
+   */
+  void removeCallback(DuckHuntListener listener) throws IllegalParameterException;
+
+  /**
+   * Changes the game settings.
+   *
+   * @param gameSettings object with the chosen game settings
+   * @throws IllegalParameterException if the gameSettings is a null reference.
+   */
+  void changeGameSettings(GameSettingsDescriptor gameSettings) throws IllegalParameterException;
 }
