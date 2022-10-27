fa13caaa9ddea84fd21d2bd0c000aee7aca2cce0
Silas-HHN <68948614+Silas-HHN@users.noreply.github.com>
2022-10-27 17:16:52 +0200

    >>Connect Four<< update.

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/connectfour/IllegalOperationException.java
@@ -0,0 +1,8 @@
+package de.hhn.it.devtools.apis.connectfour;
+
+/**
+ * This Class models an illegal operation exception, which might occur when the user tries to place
+ * a disc in a row without any empty slots.
+ */
+public class IllegalOperationException extends Throwable {
+}
