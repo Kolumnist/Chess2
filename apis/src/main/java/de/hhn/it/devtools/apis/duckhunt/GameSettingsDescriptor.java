e2dabab6e95ff8674bf29a410438434051dfa7e7
Peter Stibitz <pstibitz@stud.hs-heilbronn.de>
2022-10-27 17:59:48 +0200

    Added GameSettings Descriptor

--- apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/GameSettingsDescriptor.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/GameSettingsDescriptor.java
@@ -1,2 +1,41 @@
-package de.hhn.it.devtools.apis.duckhunt;public class GameSettingsDescriptor {
+package de.hhn.it.devtools.apis.duckhunt;
+
+/**
+ * Describes a games sound (ON/OFF), volume.
+ */
+public class GameSettingsDescriptor {
+
+  private boolean soundOn;
+  private double volume;
+
+  /**
+   * Standard Constructor leaving soundON and volume at first Null.
+   */
+  public GameSettingsDescriptor(){
+  }
+
+  public double getVolume() {
+    return volume;
+  }
+
+  public void setVolume(double volume) {
+    this.volume = volume;
+  }
+
+  public boolean isSoundOn() {
+    return soundOn;
+  }
+
+  public void setSoundOn(boolean soundOn) {
+    this.soundOn = soundOn;
+  }
+
+  @Override
+  public String toString() {
+    return "GameSettingsDescriptor{"
+         + "SoundOn=" + soundOn
+         + ", volume=" + volume
+         + '}';
+  }
+
 }
