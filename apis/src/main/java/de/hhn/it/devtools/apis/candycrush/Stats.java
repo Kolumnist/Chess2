7a9e6c968f399ac5f5a014e12c70bf2e713c013b
Leo <leonidas01032002@gmail.com>
2022-10-27 14:39:49 +0200

    Stats angepasst

--- apis/src/main/java/de/hhn/it/devtools/apis/candycrush/Stats.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/candycrush/Stats.java
@@ -1,6 +1,23 @@
 package de.hhn.it.devtools.apis.candycrush;
 
 public class Stats {
+/*
+   * Enum that describe the state of the candy´s life.
+ */
+    public enum stats {
+    /*
+     * Candy is visible and is not cruched.
+     */
+    ALIVE,
+    /*
+     * Candy is crushed and changes to a crushed candy.
+     */
+    CRUSHED,
 
+    /*
+     * Candy is dead and will be deleted.
+     */
+    DEAD
 
+}
 }
