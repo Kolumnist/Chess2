65e3ea8d09d1cbb741f6258cbc0ed82fb6496151
Jannik Döring <jdoering@stud.hs-heilbronn.de>
2022-10-27 17:25:12 +0200

    fixed variable names in GameState record

--- apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/GameState.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/duckhunt/GameState.java
@@ -2,8 +2,5 @@ package de.hhn.it.devtools.apis.duckhunt;
 
 /**
  * This Record saves information about the current games state.
- *
- * @author Jannik Döring
- * @version 1.0
  */
-public record GameState(int playerScore, int score, int level) {}
+public record GameState(int playerScore, int ammo, int round) {}
