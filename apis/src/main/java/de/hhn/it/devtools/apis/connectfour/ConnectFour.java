ec5bbac496d65a0c4f01653d73ab5a79dea1befe
Silas-HHN <68948614+Silas-HHN@users.noreply.github.com>
2022-10-27 17:14:32 +0200

    >>Connect Four<< update.

--- apis/src/main/java/de/hhn/it/devtools/apis/connectfour/ConnectFour.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/connectfour/ConnectFour.java
@@ -1,4 +1,89 @@
 package de.hhn.it.devtools.apis.connectfour;
 
+import java.util.List;
+
+/**
+ * This is an interface to the game Connect Four.
+ */
 public interface ConnectFour {
+  /**
+   * Create a new user profile.
+   *
+   * @param name name of the profile
+   * @return User profile
+   */
+  Profile createProfile(String name);
+
+  /**
+   * Change the name of the profile with the specified ID.
+   *
+   * @param profileId ID of the profil
+   * @param name      Name of the profile
+   */
+  void setProfileName(long profileId, String name) throws IllegalNameException;
+
+  /**
+   * Delete the profile with the specified ID.
+   *
+   * @param profileId ID of the profile.
+   */
+  void deleteProfile(long profileId);
+
+  /**
+   * Get a list of all user profiles.
+   *
+   * @return List of user profiles
+   */
+  List<Profile> getProfiles();
+
+  /**
+   * Change the mode of the game.
+   *
+   * @param mode Game mode
+   */
+  void setMode(Mode mode);
+
+  /**
+   * Change the difficulty of the game.
+   *
+   * @param difficulty Difficulty level
+   */
+  void setDifficulty(Difficulty difficulty);
+
+  /**
+   * Choose the profile which will be used in singleplayer mode or as the profile for player 1 in
+   * PvP mode.
+   * Player 1 begins.
+   *
+   * @param a Player 1
+   */
+  void chooseProfileA(Profile a);
+
+  /**
+   * Choose the profile, which will be used in PvP mode for player 2.
+   * Player 2 is second.
+   *
+   * @param b Player 2
+   */
+  void chooseProfileB(Profile b);
+
+  /**
+   * Start the game
+   */
+  void startGame() throws ProfileNotSelectedException;
+
+  /**
+   * Place a disc at the specified position.
+   *
+   * @param column Column in which the disc should be placed in
+   * @throws IllegalOperationException If column is full
+   */
+  void placeDiscAt(int column) throws IllegalOperationException;
+
+  /**
+   * End the current game.
+   */
+  void quitGame();
+
 }
+
