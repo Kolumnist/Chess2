e4e00db5fa1bb28d2c1d321c6e90193a9ac9c332
Leo <leonidas01032002@gmail.com>
2022-10-27 13:31:41 +0200

    Profile Class überarbeitet, Exeption eingefügt

--- apis/src/main/java/de/hhn/it/devtools/apis/candycrush/Profile.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/candycrush/Profile.java
@@ -1,5 +1,23 @@
 package de.hhn.it.devtools.apis.candycrush;
-
+/**
+ * This class models the user profile.
+ */
 public class Profile {
+    // Profile ID
+    private final long id;
+    /**
+     * Create a new user profile.
+     *
+     * @param name the name of the profile
+     */
+    public Profile(String name) {
+        id = 0;
+    }
+    /**
+     * Get the ID of the user profile. (return)
+     */
+    public long getId() {
+        return id;
+    }
 
 }
