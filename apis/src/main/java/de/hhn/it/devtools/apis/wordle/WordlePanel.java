40115c9e363c5922b36f50eaa9d0ebd6ae4a64ef
MDrautz <madrautz@stud.hs-heilbronn.de>
2022-10-28 12:41:40 +0200

    docs: added documentation to deleteLetter method in WordlePanel

--- apis/src/main/java/de/hhn/it/devtools/apis/wordle/WordlePanel.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/wordle/WordlePanel.java
@@ -24,6 +24,9 @@ public class WordlePanel {
     this.letter = letter;
   }
 
+  /**
+   * Deletes the content of the field letter.
+   */
   public void deleteLetter() {
     this.letter = ' ';
   }
