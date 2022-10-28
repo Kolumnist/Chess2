741e0f6b9a5750b3f44197d16ac334a1c287fa4d
Cspielmann <Cspielmann@stud.hs-heilbronn.de>
2022-10-28 12:33:25 +0200

    feat: updated wordleDemo with logger.

--- apis/src/test/java/wordle/WordleServiceUsageDemo.java
+++ apis/src/test/java/wordle/WordleServiceUsageDemo.java
@@ -8,25 +8,47 @@ import de.hhn.it.devtools.apis.wordle.WordleGuess;
 
 
 public class WordleServiceUsageDemo {
+
+  private static final org.slf4j.Logger logger =
+          org.slf4j.LoggerFactory.getLogger(WordleServiceUsageDemo.class);
+
   public static void main(String[] args) throws IllegalGuessException {
 
     WordleService demo = null;
-    WordleKeyListener demoListener = null;
 
     demo.startGame(); // Player starts a new game
 
     demo.selectWordle(); // System selects a Wordle for the user to guess
 
+    WordleGuess solution = new WordleGuess("Grace");
+    logger.info("create WordleGuess with parameter: Grace");
+
+    WordleGuess firstGuess = new WordleGuess("Cargo");
+    logger.info("create WordleGuess with parameter: Cargo");
+
     demo.validateWordleGuess(); // System validates the user's guess
 
-    demo.validateWordleGuess(); // throws IllegalGuessException
+    WordleGuess secondGuess = new WordleGuess("Grac");
+    logger.info("create WordleGuess with parameter: Grac");
 
-    demo.validateWordleGuess();
+    demo.validateWordleGuess(); // throws IllegalGuessException, because Guess is too short
+
+    demo.validateWordleGuess(); // Player adds 'e' to his Guess "Grace"
+    logger.info("GuessWord is equal to solution"); // Player wins Game
 
     demo.startAnotherGame();
 
     demo.selectWordle(); // a new Wordle is selected for the user to guess
 
+    WordleGuess secondSolution = new WordleGuess("Fairy");
+    logger.info("New Solution is created");
+
+    WordleGuess newGuess = new WordleGuess("Cargo");
+    logger.info("create WordleGuess with parameter: Fairy");
+
     demo.validateWordleGuess();
+    logger.info("GuessWord is equal to solution"); // Player wins Game
+
+    demo.quitGame();
   }
 }
