ce075454e127ae34f45a22e10f1983661e784453
MDrautz <madrautz@stud.hs-heilbronn.de>
2022-10-28 12:50:05 +0200

    feat: implemented IllegalGuessException

--- apis/src/main/java/de/hhn/it/devtools/apis/wordle/IllegalGuessException.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/wordle/IllegalGuessException.java
@@ -4,5 +4,10 @@ package de.hhn.it.devtools.apis.wordle;
  * Is thrown if a guess does not have the correct length.
  */
 
-public class IllegalGuessException extends Exception{
+public class IllegalGuessException extends Exception {
+
+  public IllegalGuessException(String message) {
+    super(message);
+  }
+
 }
