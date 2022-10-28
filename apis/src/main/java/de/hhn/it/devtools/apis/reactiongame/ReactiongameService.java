a9163cce9d4a83bc4d27690e2c50552a49eecaa0
Maximilian König <mkoenig2@stud.hs-heilbronn.de>
2022-10-28 15:12:46 +0200

    + added RunListener.java + added first methods to ReactiongameService.java

--- apis/src/main/java/de/hhn/it/devtools/apis/reactiongame/ReactiongameService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/reactiongame/ReactiongameService.java
@@ -1,5 +1,23 @@
 package de.hhn.it.devtools.apis.reactiongame;
 
 
+import de.hhn.it.devtools.apis.ttrpgsheets.exceptions.IllegalParameterException;
+
 public interface ReactiongameService {
+
+  void newRun(Difficulty difficulty) throws IllegalParameterException;
+
+  void endRun();
+
+  void addCallback(int id, RunListener runListener) throws IllegalParameterException;
+
+  void removeCallback(int id, RunListener runListener) throws IllegalParameterException;
+
+  void startTimer();
+
+  void pauseTimer();
+
+  void endTimer();
+
+
 }
