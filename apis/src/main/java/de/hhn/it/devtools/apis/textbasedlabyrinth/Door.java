6c76c5355e5377dc05774987418f7999fdfa7a1e
rschiess <rschiess@stud.hs-heilbronn.de>
2022-10-28 13:25:06 +0200

    checkstyle door class

--- apis/src/main/java/de/hhn/it/devtools/apis/textbasedlabyrinth/Door.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/textbasedlabyrinth/Door.java
@@ -7,66 +7,88 @@ package de.hhn.it.devtools.apis.textbasedlabyrinth;
 public class Door {
 
 
-    private Puzzle puzzle;
-    private boolean locked;
-    private boolean fake;
-    private String inspectMessage;
+  private Puzzle puzzle;
+  private boolean locked;
+  private boolean fake;
+  private String inspectMessage;
 
 
+  /**
+   * ccreates door.
+   */
+  public Door() {
+    inspectMessage = "This door is open";
+    locked = false;
+    fake = false;
+  }
 
-    public Door() {
-        inspectMessage = "This door is open";
-        locked = false;
-        fake = false;
+  /**
+   * Unlocked door.
+   *
+   * @param item gets an item if needed
+   * @return solved Puzzle
+   */
+  public boolean unlock(Item item) {
+    boolean isSolved = puzzle.setSolved(item);
+    if (isSolved) {
+      locked = false;
+      inspectMessage = "This door is open. ";
+    }
+    return isSolved;
+  }
+
+  /**
+   * Get the puzzle to description.
+   *
+   * @return get the description for the puzzle
+   */
+  public String getInspectMessage() {
+    String s = inspectMessage;
+    if (locked) {
+      s = s + puzzle.getDescription();
     }
 
+    return s;
+  }
 
-    public boolean unlock(Item item) {
-        boolean isSolved = puzzle.setSolved(item);
-        if (isSolved) {
-            locked = false;
-            inspectMessage = "This door is open. ";
-        }
-        return isSolved;
+  /**
+   * locked door.
+   *
+   * @param key item need to open
+   */
+  public void setPuzzle(Item key) {
+    this.puzzle = new Puzzle(key);
+
+    locked = true;
+    inspectMessage = "This door is locked.";
+  }
+
+  /**
+   * Open a locked door.
+   *
+   * @return door is open now
+   */
+  public String open() {
+    String s = inspectMessage;
+
+    if (locked) {
+      s = s + "" + puzzle.getDescription();
+    } else {
+      s = s + "" + "You open the door.";
     }
+    return s;
+  }
 
-    public String getInspectMessage() {
-        String s = inspectMessage;
-        if (locked) {
-            s = s + puzzle.getDescription();
-        }
+  public boolean checkIfLocked() {
+    return locked;
+  }
 
-        return s;
-    }
+  public boolean checkIfFake() {
+    return fake;
+  }
 
-    public void setPuzzle(Item key) {
-        this.puzzle = new Puzzle(key);
-
-        locked = true;
-        inspectMessage = "This door is locked.";
-    }
-
-    public String open() {
-        String s = inspectMessage;
-
-        if (locked) {
-            s = s + "" + puzzle.getDescription();
-        } else {
-            s = s + "" + "You open the door.";
-        }
-        return s;
-    }
-
-    public boolean checkIfLocked() {
-        return locked;
-    }
-
-    public boolean checkIfFake() {
-        return fake;
-    }
-
-    public Puzzle getPuzzle() {
-        return puzzle;
-    }
+  public Puzzle getPuzzle() {
+    return puzzle;
+  }
 
 }
