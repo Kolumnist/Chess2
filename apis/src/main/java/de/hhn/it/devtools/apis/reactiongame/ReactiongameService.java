ab99c520d90171b145839fe54273615f160dd8cf
Tobias Bergtold <tbergtold@stud.hs-heilbronn.de>
2022-11-03 15:36:45 +0100

    Updated the documentation and changed some methods.

--- apis/src/main/java/de/hhn/it/devtools/apis/reactiongame/ReactiongameService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/reactiongame/ReactiongameService.java
@@ -1,23 +1,40 @@
 package de.hhn.it.devtools.apis.reactiongame;
 
 
-import de.hhn.it.devtools.apis.ttrpgsheets.exceptions.IllegalParameterException;
+import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
 
 public interface ReactiongameService {
 
+  /**
+   * Starts a new run and the timer.
+   *
+   * @param difficulty sets the difficulty of the starting run.
+   * @throws IllegalParameterException if the difficulty does not exist.
+   */
   void newRun(Difficulty difficulty) throws IllegalParameterException;
 
+  /**
+   * Pauses the run and the timer.
+   */
+  void pauseRun();
+
+  /**
+   * Continues a paused run.
+   */
+  void continueRun();
+
+  /**
+   * Exit the run, to return to the main menu.
+   */
   void endRun();
 
-  void addCallback(int id, RunListener runListener) throws IllegalParameterException;
-
-  void removeCallback(int id, RunListener runListener) throws IllegalParameterException;
-
+  /**
+   * Starts the timer when a new run starts.
+   */
   void startTimer();
 
+  /**
+   * Pauses the timer when a run is paused.
+   */
   void pauseTimer();
-
-  void endTimer();
-
-
 }
