36e139f29fc9312d83ad87877d6f695a52c8990f
GitBrainLOL <97748725+GitBrainLOL@users.noreply.github.com>
2022-11-03 17:13:45 +0100

    Changed what Herr Stoltenberg said

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/minesweeper/MinesweeperActionListener.java
@@ -0,0 +1,11 @@
+package de.hhn.it.devtools.apis.minesweeper;
+
+/**
+ * Observer to handle events
+ * @author Lara Weller, Jason Herrmann
+ * @version 0.1
+ */
+
+public interface MinesweeperActionListener{
+    public void onAction(String event);
+}
--- apis/src/test/java/minesweeper/DemoMinesweeperUsage.java
+++ apis/src/test/java/minesweeper/DemoMinesweeperUsage.java
@@ -1,52 +1,72 @@
 package minesweeper;
 
+import de.hhn.it.devtools.apis.minesweeper.Difficulty;
+import de.hhn.it.devtools.apis.minesweeper.MinesweeperActionListener;
+
 /**
  * The Handler interface is for handling interactions between a player with a component
  * @author Lara Weller, Jason Herrmann
  * @version 0.1
  */
 
-import de.hhn.it.devtools.apis.minesweeper.Handler;
+import de.hhn.it.devtools.apis.minesweeper.MinesweeperService;
+    
+public class DemoMinesweeperUsage implements MinesweeperService{
 
-public class DemoMinesweeperUsage implements Handler{
-
-    public static void main(String[]args){
-
-        DemoMinesweeperUsage demo= new DemoMinesweeperUsage();
-
+    private MinesweeperActionListener minesweeperActionListener;
+       
+    public static void main(String[]args){        
+        DemoMinesweeperUsage demo = new DemoMinesweeperUsage();
+        
+        demo.setMinesweeperActionListener(
+            new MinesweeperActionListener(){                         
+                public void onAction(String event){                    
+                    System.out.println("Starting the long running task.");
+                }
+        });
+        
         // User chooses difficulty easy
-        demo.chooseDifficulty('e');
+        demo.chooseDifficulty(Difficulty.EASY);
+        System.out.println("Changing diffictulty to easy");
 
-        // Player starts a new game with fieldsize 10, 600 seconds and 20 mines
+        // Player decides to adjust some settings manually, 
+        // then starts a new game with fieldsize 10, 600 seconds and 20 mines
         demo.startGame(10, 600, 20);
-
-        // Player opens color menu to choose a different color
-        demo.changeColor();
+        System.out.println("Staring Game");
 
         // Player clicks on fields
         demo.clickField();
+        System.out.println("Field Opened");
+        System.out.println("No mine detected");
         demo.clickField();
+        System.out.println("Field Opened");
+        System.out.println("No mine detected");
         demo.clickField();
+        System.out.println("Field Opened");
+        System.out.println("No mine detected");
         demo.clickField();
+        System.out.println("Field Opened");
+        System.out.println("No mine detected");
         demo.clickField();
+        System.out.println("Field Opened");
+        System.out.println("No mine detected");
         demo.clickField();
+        System.out.println("Field Opened");
+        System.out.println("No mine detected");
 
         // Player marks field
         demo.markField();
+        System.out.println("marked field");
 
         // Player goes to the menu
         demo.menu();
+        System.out.println("Going back to the menu");
 
+        demo.minesweeperActionListener.onAction("Hello");
     }
 
     @Override
     public void startGame(int fieldSize, int time, int mineCount) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void chooseDifficulty(char difficulty) {
         // TODO Auto-generated method stub
         
     }
@@ -64,12 +84,6 @@ public class DemoMinesweeperUsage implements Handler{
     }
 
     @Override
-    public void changeColor() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
     public void clickField() {
         // TODO Auto-generated method stub
         
@@ -79,5 +93,16 @@ public class DemoMinesweeperUsage implements Handler{
     public void markField() {
         // TODO Auto-generated method stub
         
+    }
+
+	@Override
+	public void chooseDifficulty(Difficulty difficulty) {
+		// TODO Auto-generated method stub
+		
+	}
+    @Override
+    public void setMinesweeperActionListener(MinesweeperActionListener minesweeperActionListener) {
+        // TODO Auto-generated method stub
+        this.minesweeperActionListener = minesweeperActionListener;
     }
 }
