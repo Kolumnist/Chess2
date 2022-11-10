4464be8b2e8a0ebd6fc567131a8bf1afdaeffbe3
alschwab <alschwab@stud.hs-heilbronn.de>
2022-11-10 12:29:36 +0100

    BlockGrid added some Methods

--- apis/src/main/java/de/hhn/it/devtools/apis/candycrush/BlockGrid.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/candycrush/BlockGrid.java
@@ -1,15 +1,24 @@
 package de.hhn.it.devtools.apis.candycrush;
 
 public class BlockGrid {
-
-    private int target;
-    private int numRows;
-    private int numCols;
-    private char[][] map;
+    private Block [][] blocks;
 
 
-    //Constructor
-    public BlockGrid(int numRows, int numCols, int target, char[][] map) {
+    /**
+     *Create a BlockGrid with empty blocks.
+     */
+    public BlockGrid() {
+
+    }
+
+    public void setBlocks(Block[][] blocks) {
+        this.blocks = blocks;
+    }
+
+    public Block [][] getBlocks() {
+        return blocks;
+    }
+    public void changeColorOfBlocks(int xPos, int yPos , String  color) {
 
     }
 }
