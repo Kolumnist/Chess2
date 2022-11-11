debbf101ea399896f7405fed728179ae5cfea9b2
Tobias Bergtold <tbergtold@stud.hs-heilbronn.de>
2022-11-11 15:41:29 +0100

    Deleted RunListener.java. Added Methods to ReactiongameListener.

--- apis/src/main/java/de/hhn/it/devtools/apis/reactiongame/ReactiongameListener.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/reactiongame/ReactiongameListener.java
@@ -1,4 +1,11 @@
 package de.hhn.it.devtools.apis.reactiongame;
 
 public interface ReactiongameListener {
+  void addCircle(Circle circle);
+
+  void removeCircle(Circle circleid);
+
+  void addObstacle(Obstacle obstacle);
+
+  void removeObstacle(Obstacle obstacleid);
 }
