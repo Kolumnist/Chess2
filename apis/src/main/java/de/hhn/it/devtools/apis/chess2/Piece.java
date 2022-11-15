17a4e37be4903039ad96b16987960ae67170a273
Lara Mangi <crazyjasmin9@gmail.com>
2022-11-15 12:59:38 +0100

    change interface into abstract piece class and change also some methods a little bit

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/Piece.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/Piece.java
@@ -1,37 +1,51 @@
 package de.hhn.it.devtools.apis.chess2;
 
-import java.util.ArrayList;
-
 /**
- * This interface defines attributes of every piece.
+ * This abstract class defines attributes of every piece.
  *
  * @author Collin, Lara, Michel
  * @version 1.0
  */
 
-public interface Piece {
+public abstract class Piece {
 
-  /** Holds all possible x,y coordinates of the piece where it can move. */
+  /**
+   * Holds all possible x,y coordinates of the piece where it can move.
+   */
   Coordinate[] possibleMoves = new Coordinate[0];
 
-  /** Current position of the piece. */
+  /**
+   * Current position of the piece.
+   */
   Coordinate coordinate = new Coordinate();
+
+  /**
+   * gives the piece a color
+   */
+  char color;
+
 
   /**
    * All possible moves are returned as a List of x, y coordinates.
    *
    * @return possibleMoves of the current piece
    */
-  Coordinate[] getPossibleMove();
+  Coordinate[] getPossibleMove() {
+    return possibleMoves;
+  }
 
-  /*private void calculate() {
+  /*private void calculate() {   -> muss in die einzelnen Piece Klassen
 
   }*/
 
   /**
-   *  Sets the new coordinate.
+   * Sets the new coordinate.
    *
    * @param coordinate is the new coordinate
    */
-  void setCoordinate(Coordinate coordinate);
+  void setCoordinate(Coordinate coordinate) {
+    this.coordinate = coordinate;
+  }
 }
+
+
