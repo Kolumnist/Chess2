50b61873413ae9451a8e8fd5e6fdfe6c16525e0b
Lara Mangi <crazyjasmin9@gmail.com>
2022-11-15 14:26:05 +0100

    implement method calculate into class elephant

--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Elephant.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Elephant.java
@@ -1,5 +1,9 @@
 package de.hhn.it.devtools.components.chess2.pieces;
 
-public class Elephant {
+import de.hhn.it.devtools.apis.chess2.Piece;
 
+public class Elephant extends Piece {
+
+  public void calculate() {
+  }
 }
