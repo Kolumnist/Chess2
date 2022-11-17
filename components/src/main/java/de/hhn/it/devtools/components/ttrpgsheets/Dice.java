8f96deb70f8b7de0788e7a281071da7054fb1412
Gabriel Schmidlin Kitzig <gschmidlin@stud.hs-heilbronn.de>
2022-11-17 15:24:17 +0100

    Added the constructor and the methods.

--- components/src/main/java/de/hhn/it/devtools/components/ttrpgsheets/Dice.java
+++ components/src/main/java/de/hhn/it/devtools/components/ttrpgsheets/Dice.java
@@ -1,4 +1,50 @@
 package de.hhn.it.devtools.components.ttrpgsheets;
 
+import java.util.Random;
+import de.hhn.it.devtools.apis.ttrpgsheets.DiceDescriptor;
+
+/**
+ * The class Dice for the default Character Sheet.
+ */
 public class Dice {
+  private int size;
+  private int value;
+  Random generator = new Random();
+
+  /**
+   * Constructor stating diceTyp of {@link DiceDescriptor} and value of the Dice throw the method getResult().
+   *
+   * @param diceTyp type of the Dice
+   *
+   */
+  public Dice(DiceDescriptor diceTyp) {
+    switch (diceTyp.getDiceType()) {
+      case D2 -> size = 2;
+      case D4 -> size = 4;
+      case D6 -> size = 6;
+      case D8 -> size = 8;
+      case D10 -> size = 10;
+      case D12 -> size = 12;
+      case D20 -> size = 20;
+      case D100 -> size = 100;
+    }
+    value = diceTyp.getResult();
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public int rollDice(){
+    value = generator.nextInt(size) + 1;
+    return value;
+  }
+
+  public void changeSize(int newSize){
+    size=newSize;
+  }
 }
