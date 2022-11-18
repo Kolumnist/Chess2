5f459fa658262c9189a82c14525ac1741b624f9c
Maximilian König <maximilian.koenig@mein.gmx>
2022-11-18 15:50:49 +0100

    + updated DemoReactiongameUsage.java

--- apis/src/test/java/reactiongame/DemoReactiongameUsage.java
+++ apis/src/test/java/reactiongame/DemoReactiongameUsage.java
@@ -1,7 +1,10 @@
 package reactiongame;
 
 import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
+import de.hhn.it.devtools.apis.reactiongame.AimTargetDescriptor;
 import de.hhn.it.devtools.apis.reactiongame.Difficulty;
+import de.hhn.it.devtools.apis.reactiongame.ObstacleDescriptor;
+import de.hhn.it.devtools.apis.reactiongame.ReactiongameListener;
 import de.hhn.it.devtools.apis.reactiongame.ReactiongameService;
 
 /**
@@ -14,16 +17,28 @@ public class DemoReactiongameUsage {
     public static void main(String[] args) throws IllegalParameterException {
 
         ReactiongameService reactiongameService = null;
+        ReactiongameListener reactiongameListener = null;
+
 
 
         logger.info("> Player starts a run");
         reactiongameService.newRun(Difficulty.HARD);
+        reactiongameListener.addObstacle(new ObstacleDescriptor(100, 100, 200, 200));
+        reactiongameListener.addAimTarget(new AimTargetDescriptor(400, 400, 50, 'W'));
 
         logger.info("> Player pauses the run");
         reactiongameService.pauseRun();
 
         logger.info("> Player continues the run");
         reactiongameService.continueRun();
+
+
+        logger.info("> Player hits obstacle 0 AND loses a life");
+        reactiongameListener.hitObstacle(0);
+        reactiongameListener.currentLife(2);
+
+        logger.info("> Player gets to aim target");
+        reactiongameListener.aimTargetHit('W', 0);
 
         logger.info("> Time is up");
         reactiongameService.endRun();
