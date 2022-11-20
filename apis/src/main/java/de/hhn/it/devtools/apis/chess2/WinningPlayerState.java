e089bada4675c9144b2bb922374cdf731094cc5b
Lara Mangi <crazyjasmin9@gmail.com>
2022-11-20 15:10:19 +0100

    add a new enum class for the winning states

---
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/WinningPlayerState.java
@@ -0,0 +1,18 @@
+package de.hhn.it.devtools.apis.chess2;
+
+/**
+ * Enum to describe the different fields and if there is something on it or not.
+ *
+ * @author Collin, Lara, Michel
+ * @version 1.1
+ */
+
+public enum WinningPlayerState {
+
+  /** Is a Jail Field and has a King on it. */
+  BLACK_WIN,
+  /** Is a Jail Field and has a King on it. */
+  WHITE_WIN,
+  /** Is a Jail Field and has a King on it. */
+  STILL_RUN;
+    }
