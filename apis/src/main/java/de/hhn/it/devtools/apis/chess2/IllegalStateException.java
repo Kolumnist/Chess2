d833e8c4bc6a0231c755281bafa0f4d60321e3ff
Collin <96417832+Kolumnist@users.noreply.github.com>
2022-11-23 14:21:29 +0100

    jdoc: checkstyle now valid or without problems

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/IllegalStateException.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/IllegalStateException.java
@@ -7,55 +7,56 @@ package de.hhn.it.devtools.apis.chess2;
  * @version 1.0
  */
 
-public class IllegalStateException extends Exception{
+public class IllegalStateException extends Exception {
+
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(IllegalStateException.class);
 
   /**
-   * Constructs a new exception with {@code null} as its detail message.
-   * The cause is not initialized, and may subsequently be initialized by a
-   * call to {@link #initCause}.
+   * Constructs a new exception with {@code null} as its detail message. The cause is not
+   * initialized, and may subsequently be initialized by a call to {@link #initCause}.
    */
-  public IllegalStateException(){}
+  public IllegalStateException() {
+  }
 
   /**
-   * Constructs a new exception with the specified detail message.  The
-   * cause is not initialized, and may subsequently be initialized by
-   * a call to {@link #initCause}.
+   * Constructs a new exception with the specified detail message.  The cause is not initialized,
+   * and may subsequently be initialized by a call to {@link #initCause}.
    *
-   * @param message the detail message. The detail message is saved for
-   *                later retrieval by the {@link #getMessage()} method.
+   * @param message the detail message. The detail message is saved for later retrieval by the
+   *                {@link #getMessage()} method.
    */
-  public IllegalStateException(final String message){super(message); }
+  public IllegalStateException(final String message) {
+    super(message);
+  }
 
   /**
-   * Constructs a new exception with the specified cause and a detail
-   * message of <code>(cause==null ? null : cause.toString())</code> (which
-   * typically contains the class and detail message of <code>cause</code>).
-   * This constructor is useful for exceptions that are little more than
+   * Constructs a new exception with the specified cause and a detail message of <code>(cause==null
+   * ? null : cause.toString())</code> (which typically contains the class and detail message of
+   * <code>cause</code>). This constructor is useful for exceptions that are little more than
    * wrappers for other throwables.
    *
-   * @param cause the cause (which is saved for later retrieval by the
-   *              {@link #getCause()} method).  (A <code>null</code> value is
-   *              permitted, and indicates that the cause is nonexistent or
-   *              unknown.)
+   * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method).
+   *              (A <code>null</code> value is permitted, and indicates that the cause is
+   *              nonexistent or unknown.)
    */
-  public IllegalStateException(final Throwable cause){super(cause); }
+  public IllegalStateException(final Throwable cause) {
+    super(cause);
+  }
 
   /**
-   * Constructs a new exception with the specified detail message and
-   * cause.
+   * Constructs a new exception with the specified detail message and cause.
    *
    * <p>Note that the detail message associated with
-   * {@code cause} is <i>not</i> automatically incorporated in
-   * this exception's detail message.
+   * {@code cause} is <i>not</i> automatically incorporated in this exception's detail message.
    *
-   * @param message the detail message (which is saved for later retrieval
-   *                by the {@link #getMessage()} method).
-   * @param cause   the cause (which is saved for later retrieval by the
-   *                {@link #getCause()} method).  (A <code>null</code> value is
-   *                permitted, and indicates that the cause is nonexistent or
-   *                unknown.)
+   * @param message the detail message (which is saved for later retrieval by the
+   *                {@link #getMessage()} method).
+   * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()}
+   *                method).  (A <code>null</code> value is permitted, and indicates that the cause
+   *                is nonexistent or unknown.)
    */
-  public IllegalStateException(final String message, final Throwable cause){super(message, cause);}
+  public IllegalStateException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
 }
