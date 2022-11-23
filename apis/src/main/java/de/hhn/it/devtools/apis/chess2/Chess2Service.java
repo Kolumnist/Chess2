158dbec3808022f3e25f6be6a042c8d527aa00a6
Michel Jouaux <mjouaux@stud.hs-heilbronn.de>
2022-11-23 15:07:30 +0100

    Doc: jdoc updated in the class Chess2Service.java

--- apis/src/main/java/de/hhn/it/devtools/apis/chess2/Chess2Service.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/chess2/Chess2Service.java
@@ -20,8 +20,10 @@ public interface Chess2Service {
 
   /**
    * Destroys an instance of ChessGame and brings scene back to main menu.
+   *
+   * @throws IllegalStateException if the method gets called during a wrong state
    */
-  void endGame();
+  void endGame() throws IllegalStateException;
 
   /**
    * Calls the method showWinningPlayer and after that calls reset or returns to the main menu.
@@ -41,9 +43,10 @@ public interface Chess2Service {
    * @param selectedPieceCoordinate Coordinate of the position of the selected Piece
    * @return Coordinate[] all positions that the selected Piece can move to
    * @throws IllegalParameterException if piecePos is a null reference or incomplete
+   * @throws IllegalStateException     if the method gets called during a wrong state
    */
   Coordinate[] getPossibleMoves(Coordinate selectedPieceCoordinate)
-      throws IllegalParameterException;
+      throws IllegalParameterException, IllegalParameterException;
 
   /**
    * Changes the position of the selected piece.
@@ -51,9 +54,10 @@ public interface Chess2Service {
    * @param selectedCoordinate the position of the selected piece
    * @param newCoordinate      the new position of the piece
    * @throws IllegalParameterException if newPos is a null reference or incomplete
+   * @throws IllegalStateException     if the method gets called during a wrong state
    */
   void moveSelectedPiece(Coordinate selectedCoordinate, Coordinate newCoordinate)
-      throws IllegalParameterException, InvalidMoveException;
+      throws IllegalParameterException, InvalidMoveException, IllegalStateException;
 
   /**
    * Returns the FieldState of the selected field.
@@ -61,12 +65,14 @@ public interface Chess2Service {
    * @param selectedCoordinate the position of the selected Field
    * @return FieldState of the selected field
    * @throws IllegalParameterException if pos is a null reference or incomplete
+   * @throws IllegalStateException     if the method gets called during a wrong state
    */
-  FieldState getFieldState(Coordinate selectedCoordinate) throws IllegalParameterException;
+  FieldState getFieldState(Coordinate selectedCoordinate)
+      throws IllegalParameterException, IllegalStateException;
 
   /**
-   * Returns BLACK_WIN, WHITE_WIN, NO_WINNER as an Enum that defines which player won or if there
-   * is none.
+   * Returns BLACK_WIN, WHITE_WIN, NO_WINNER, STILL_RUNNING as an Enum that defines which player won
+   * or if there is none.
    *
    * @return Enum which defines the player that won or if there is none
    */
