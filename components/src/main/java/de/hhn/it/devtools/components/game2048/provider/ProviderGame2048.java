0ebea89b0d6ba104ddf1e176edaa81f1360c7b68
Nicolas Holl <nicolas.holl23@web.de>
2022-11-23 19:02:40 +0100

    Deleted methods in facade that do belong to the component

---
+++ components/src/main/java/de/hhn/it/devtools/components/game2048/provider/ProviderGame2048.java
@@ -0,0 +1,16 @@
+package de.hhn.it.devtools.components.game2048.provider;
+
+import de.hhn.it.devtools.apis.game2048.Game2048Listener;
+import de.hhn.it.devtools.apis.game2048.Game2048Service;
+
+public class ProviderGame2048 implements Game2048Service {
+  @Override
+  public void addCallback(Game2048Listener listener) {
+
+  }
+
+  @Override
+  public void removeCallback(Game2048Listener listener) {
+
+  }
+}
