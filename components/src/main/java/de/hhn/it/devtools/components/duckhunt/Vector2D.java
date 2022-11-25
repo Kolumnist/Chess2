9f382da205a6cc66392e8e453354f9a02d674d8e
Hannes Jetter <hannes.jetter@gmail.com>
2022-11-25 03:02:43 +0100

    added methods to Vector2D Type

--- components/src/main/java/de/hhn/it/devtools/components/duckhunt/Vector2D.java
+++ components/src/main/java/de/hhn/it/devtools/components/duckhunt/Vector2D.java
@@ -1,5 +1,7 @@
 package de.hhn.it.devtools.components.duckhunt;
 
+import java.util.Objects;
+
 /**
  * Class for 2-dimensional vectors.
  */
@@ -7,6 +9,15 @@ public class Vector2D {
   private int x;
   private int y;
 
+  public Vector2D() {
+    this.x = 0;
+    this.y = 0;
+  }
+
+  public Vector2D(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
 
   public int getX() {
     return x;
@@ -22,5 +33,27 @@ public class Vector2D {
 
   public void setY(int y) {
     this.y = y;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Vector2D vector2D = (Vector2D) o;
+    return getX() == vector2D.getX() && getY() == vector2D.getY();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getX(), getY());
+  }
+
+  @Override
+  public String toString() {
+    return "Vector2D{" + "x=" + x + ", y=" + y + '}';
   }
 }
