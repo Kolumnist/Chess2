c4bc33fa9bbcc6e1f22a54207a844af8f7fb1f9f
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2022-11-25 15:52:28 +0100

    Added good cases and bad cases test classes for all component classes

---
+++ components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestDescriptionGoodCases.java
@@ -0,0 +1,24 @@
+package de.hhn.it.devtools.components.ttrpgsheets.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestDescriptionGoodCases {
+
+  @Test
+  void getType() {
+  }
+
+  @Test
+  void getDescription() {
+  }
+
+  @Test
+  void setDescription() {
+  }
+
+  @Test
+  void testToString() {
+  }
+}
---
+++ components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestStatGoodCases.java
@@ -0,0 +1,60 @@
+package de.hhn.it.devtools.components.ttrpgsheets.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestStatGoodCases {
+
+  @Test
+  void getTotalValue() {
+  }
+
+  @Test
+  void addAbilityPoint() {
+  }
+
+  @Test
+  void removeAbilityPoint() {
+  }
+
+  @Test
+  void getType() {
+  }
+
+  @Test
+  void getBaseValue() {
+  }
+
+  @Test
+  void getAbilityPointsUsed() {
+  }
+
+  @Test
+  void setAbilityPointsUsed() {
+  }
+
+  @Test
+  void getOffset() {
+  }
+
+  @Test
+  void getMiscellaneous() {
+  }
+
+  @Test
+  void setMiscellaneous() {
+  }
+
+  @Test
+  void isLevelStat() {
+  }
+
+  @Test
+  void setLevelStat() {
+  }
+
+  @Test
+  void testToString() {
+  }
+}
