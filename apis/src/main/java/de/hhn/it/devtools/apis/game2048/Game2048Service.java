d22710e055ef2d2b2f70492b1df410caa603f7db
Nicolas Holl <nicolas.holl23@web.de>
2022-11-25 16:18:42 +0100

    Implemented discussed changes

--- apis/src/main/java/de/hhn/it/devtools/apis/game2048/Game2048Service.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/game2048/Game2048Service.java
@@ -9,6 +9,28 @@ import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
  */
 public interface Game2048Service {
 
+  /**
+   * This methode will be called to start a new game.
+   * It happens when the Application starts to run or when the reset Button in the Frontend gets
+   * pressed. This Button will be used after a game is won or if an unfinished game should be restarted.
+   *
+   * It checks if there are Blocks on the Game board, they will be deleted.
+   * Afterwards 2 Blocks will be placed on the Game board.
+   */
+  void initialisation();
+
+  /**
+   * This Methode will be called after one of the arrow keys got pressed.
+   * Because of that the Frontend will be notified through a listener and calls this methode in
+   * the Backend.
+   * This Methode moves a Block in the given direction, until it collides with either a border
+   * or a Block.
+   *
+   * @param direction direction in witch the Block moves
+   * @throws IllegalParameterException if move is invalid, or if one of the parameters is null
+   */
+  void moveBlocks(MovingDirection direction) throws IllegalParameterException;
+
 
   /**
    * Adds a listener to updates on the state of the game 2048.
