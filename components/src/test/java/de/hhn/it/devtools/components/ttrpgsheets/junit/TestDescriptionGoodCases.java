34286fd10e5a77646306fff9b00dd8d0c13476de
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2022-11-26 18:44:42 +0100

    Added tests for TestDescriptionGoodCases

--- components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestDescriptionGoodCases.java
+++ components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestDescriptionGoodCases.java
@@ -1,24 +1,54 @@
 package de.hhn.it.devtools.components.ttrpgsheets.junit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import de.hhn.it.devtools.apis.ttrpgsheets.DescriptionDescriptor;
+import de.hhn.it.devtools.apis.ttrpgsheets.DescriptionType;
+import de.hhn.it.devtools.components.ttrpgsheets.Description;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Test class for {@link Description} with good Cases.
+ */
+@DisplayName("Test the description with good cases.")
+public class TestDescriptionGoodCases {
 
-class TestDescriptionGoodCases {
+  static Description ageDescription;
+
+  @BeforeAll
+  static void setup() {
+    ageDescription = new Description(new DescriptionDescriptor(DescriptionType.AGE, "35"));
+  }
 
   @Test
+  @DisplayName("A new description is created.")
+  void createNewDescriptionWithDescriptor() {
+    Description description = new Description(
+            new DescriptionDescriptor(DescriptionType.CHARACTER_NAME, "Sylas"));
+    assertNotNull(description);
+    assertEquals("Sylas", description.getDescription());
+    assertEquals(DescriptionType.PLAYER_NAME, description.getType());
+  }
+
+  @Test
+  @DisplayName("Get the type of the description.")
   void getType() {
+    assertEquals(DescriptionType.AGE, ageDescription.getType());
   }
 
   @Test
+  @DisplayName("Get the description.")
   void getDescription() {
+    assertEquals("35", ageDescription.getDescription());
   }
 
   @Test
+  @DisplayName("Set the description.")
   void setDescription() {
-  }
-
-  @Test
-  void testToString() {
+    ageDescription.setDescription("36");
+    assertEquals("36", ageDescription.getDescription());
   }
 }
