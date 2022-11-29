bceccf2f62b0e9bc7b88f9f106497cf7d0b0fa85
Shira Schäfer <sschaefer@stud.hs-heilbronn.de>
2022-11-29 16:24:05 +0100

    added listener test for picturecard

--- components/src/test/java/de/hhn/it/devtools/components/memory/junit/TestPictureCard.java
+++ components/src/test/java/de/hhn/it/devtools/components/memory/junit/TestPictureCard.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPictureCard {
     private static final org.slf4j.Logger logger =
@@ -27,16 +27,63 @@ public class TestPictureCard {
     }
 
     @Test
-    @DisplayName("Test registration of callback.")
+    @DisplayName("Test registration of callback listener.")
     void CheckSuccessfulRegistrationOfCallback() throws IllegalParameterException,
             InterruptedException {
         SimplePictureCardListener listener = new SimplePictureCardListener();
         pictureCard.addCallback(listener);
-        pictureCard.turnCard();                             //changes State to visible
-        assertEquals(1, listener.states.size());    //should be one State - visible
+        pictureCard.turnCard();                             //changes State to Visible
+        assertEquals(1, listener.states.size());    //should be one State - Visible
+
     }
 
+    @Test
+    @DisplayName("Test registration of two listeners which both get notified.")
+    void CheckRegistrationTwoListeners() throws IllegalParameterException, InterruptedException {
+        SimplePictureCardListener listener0 = new SimplePictureCardListener();
+        SimplePictureCardListener listener1 = new SimplePictureCardListener();
+        pictureCard.addCallback(listener0);
+        pictureCard.addCallback(listener1);
+        pictureCard.turnCard();
+        assertAll(
+                () -> assertEquals(1,listener0.states.size()),
+                () -> assertEquals(1, listener1.states.size())
+        );
+    }
 
+    @Test
+    @DisplayName("Register one listener, remove the listener, provoke a new state.")
+    void registerAndRemoveOneListener() throws IllegalParameterException, InterruptedException {
+        SimplePictureCardListener listener = new SimplePictureCardListener();
+        pictureCard.addCallback(listener);
+        pictureCard.removeCallback(listener);
+        pictureCard.turnCard();
+        assertEquals(0, listener.states.size()); //should be 0, because removed before new state
+    }
+
+    @Test
+    @DisplayName("The same listener cannot be registered twice.")
+    void tryRegisterSameListenerTwice() throws IllegalParameterException {
+        SimplePictureCardListener listener = new SimplePictureCardListener();
+        pictureCard.addCallback(listener);
+        IllegalParameterException exception = assertThrows(IllegalParameterException.class,
+                () -> pictureCard.addCallback(listener));
+    }
+
+    @Test
+    @DisplayName("Listener which is not registered cannot be removed.")
+    void tryRemoveNotRegisteredListener() throws  IllegalParameterException {
+        SimplePictureCardListener listener = new SimplePictureCardListener();
+        IllegalParameterException exception = assertThrows(IllegalParameterException.class,
+                () -> pictureCard.removeCallback(listener));
+    }
+
+    @Test
+    @DisplayName("Null references cannot be registered as listener.")
+    void tryRegisterNullReferencesAsListener() {
+        IllegalParameterException exception = assertThrows(IllegalParameterException.class,
+                () -> pictureCard.addCallback(null));
+    }
 
 
     /**
