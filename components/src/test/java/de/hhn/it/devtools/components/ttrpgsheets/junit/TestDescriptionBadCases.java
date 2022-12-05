0985ee51b3afa34dde16e20f0aed5dcd08afa1d0
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2022-12-05 13:42:01 +0100

    Added tests for TestDescriptionBadCases

--- components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestDescriptionBadCases.java
+++ components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestDescriptionBadCases.java
@@ -1,24 +1,41 @@
 package de.hhn.it.devtools.components.ttrpgsheets.junit;
 
+import de.hhn.it.devtools.apis.ttrpgsheets.DescriptionDescriptor;
+import de.hhn.it.devtools.apis.ttrpgsheets.DescriptionType;
+import de.hhn.it.devtools.components.ttrpgsheets.Description;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class TestDescriptionBadCases {
 
+  static Description characterClassDescription;
+
+  @BeforeEach
+  void setup() {
+    characterClassDescription = new Description(
+            new DescriptionDescriptor(DescriptionType.CHARACTER_CLASS, "Warrior"));
+  }
+
   @Test
+  @DisplayName("Get the wrong type of the description.")
   void getType() {
+    assertNotEquals(DescriptionType.AGE, characterClassDescription.getType());
   }
 
   @Test
+  @DisplayName("Get the wrong description.")
   void getDescription() {
+    assertNotEquals("Mage", characterClassDescription.getDescription());
   }
 
   @Test
+  @DisplayName("Set description didn't work.")
   void setDescription() {
-  }
-
-  @Test
-  void testToString() {
+    String oldDescription = characterClassDescription.getDescription();
+    characterClassDescription.setDescription("Bard");
+    assertNotEquals(oldDescription, characterClassDescription.getDescription());
   }
 }
