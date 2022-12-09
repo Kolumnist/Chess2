5162d68f29aa8f6736f0c2ededfb71db1197227d
Michel Jouaux <mjouaux@stud.hs-heilbronn.de>
2022-12-09 13:02:41 +0100

    Feat: Added a constructor for the class Bear.java and the method calculate

--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Bear.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Bear.java
@@ -1,5 +1,36 @@
 package de.hhn.it.devtools.components.chess2.pieces;
 
-public class Bear {
+import de.hhn.it.devtools.apis.chess2.Coordinate;
+import de.hhn.it.devtools.apis.chess2.Piece;
+import java.util.ArrayList;
 
+public class Bear extends Piece {
+
+  Bear(char color, Coordinate coordinate) {
+    this.color = color;
+    this.coordinate = coordinate;
+  }
+
+  @Override
+  protected void calculate() {
+    possibleMoves = new Coordinate[9];
+    int k = 0;
+    for (int i = coordinate.getX() - 1; i < coordinate.getX() + 1; i++) {
+      for (int j = coordinate.getY() - 1; j < coordinate.getY() + 1; j++) {
+        possibleMoves[k++] = new Coordinate(i, j);
+      }
+    }
+
+    ArrayList<Integer> index = new ArrayList<>();
+    for (int i = 0; i < possibleMoves.length; i++) {
+      if ((possibleMoves[i].getX() == coordinate.getX() && possibleMoves[i].getY() == coordinate.getY())
+          || possibleMoves[i].getY() < 0
+          || possibleMoves[i].getX() < 0
+          || possibleMoves[i].getY() > 7
+          || possibleMoves[i].getX() > 7) {
+        index.add(i);
+      }
+    }
+    possibleMoves = shortenCoordinateArray(possibleMoves, index);
+  }
 }
