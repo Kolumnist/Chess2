13cbfa122e0f62e00cdde0f8ebcac23a59dcfb1b
Patrick Franz <patrick.franz@franz-vs.de>
2022-12-09 16:22:55 +0100

    fix: changed import directory

--- components/src/main/java/de/hhn/it/devtools/components/wordle/provider/panelstates/PanelState.java
+++ components/src/main/java/de/hhn/it/devtools/components/wordle/provider/panelstates/PanelState.java
@@ -1,7 +1,7 @@
 package de.hhn.it.devtools.components.wordle.provider.panelstates;
 
 import de.hhn.it.devtools.apis.wordle.State;
-import de.hhn.it.devtools.apis.wordle.WordlePanel;
+import de.hhn.it.devtools.components.wordle.provider.WordlePanel;
 
 public abstract class PanelState {
 
