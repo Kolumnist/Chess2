220f1177cc91c6f272a7191df45bf487e33b58fa
MDrautz <madrautz@stud.hs-heilbronn.de>
2022-12-09 15:15:50 +0100

    feat: added abstract class PanelState

---
+++ components/src/main/java/de/hhn/it/devtools/components/wordle/provider/panelstates/PanelState.java
@@ -0,0 +1,22 @@
+package de.hhn.it.devtools.components.wordle.provider.panelstates;
+
+import de.hhn.it.devtools.apis.wordle.State;
+import de.hhn.it.devtools.apis.wordle.WordlePanel;
+
+public abstract class PanelState {
+
+  protected State state;
+  protected WordlePanel panel;
+
+  public PanelState(WordlePanel panel){
+    this.panel = panel;
+  }
+
+  public State getState() {
+    return state;
+  }
+
+  public void setState(final State state) {
+    this.state = state;
+  }
+}
