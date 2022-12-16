05e8d46c5bd95defddcd3f3e3550bfd9f5725c74
Jonas Gierlich <jgierlich@stud.hs-heilbronn.de>
2022-12-16 14:48:16 +0100

    put the methods to add/remove obstacles and targets into the level class.

--- components/src/main/java/de/hhn/it/devtools/components/reactiongame/provider/Obstacle.java
+++ components/src/main/java/de/hhn/it/devtools/components/reactiongame/provider/Obstacle.java
@@ -1,34 +1,5 @@
 package de.hhn.it.devtools.components.reactiongame.provider;
 
-import de.hhn.it.devtools.apis.reactiongame.ObstacleDescriptor;
-
 public class Obstacle {
-
-  /**
-   * Adds an obstacle to the ui.
-   *
-   * @param obstacle obstacle
-   */
-  void addObstacle(ObstacleDescriptor obstacle) {
-
-  }
-
-  /**
-   * Removes obstacle with the given id.
-   *
-   * @param obstacleId identifier
-   */
-  void removeObstacle(int obstacleId) {
-
-  }
-
-  /**
-   * Player hits an obstacle.
-   *
-   * @param obstacleId identifier of obstacle which is hit
-   */
-  void hitObstacle(int obstacleId) {
-
-  }
 
 }
