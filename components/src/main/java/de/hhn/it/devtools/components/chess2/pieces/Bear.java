b6526ebe279d5fe09c03574dfab3f42477e11d87
Michel Jouaux <mjouaux@stud.hs-heilbronn.de>
2022-12-16 14:54:33 +0100

    Fix: Changed the constructor of Bear.java to use the super constructor

--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Bear.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Bear.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 public class Bear extends Piece {
 
   Bear(char color, Coordinate coordinate) {
-    this.color = color;
-    this.coordinate = coordinate;
+    super(color, coordinate);
   }
 
   @Override
