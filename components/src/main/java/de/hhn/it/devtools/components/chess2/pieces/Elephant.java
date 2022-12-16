4223431f29791966c5666997da76b7d9f1da152a
Lara Mangi <crazyjasmin9@gmail.com>
2022-12-16 14:58:53 +0100

    doc: create constructor for the piece classes

--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Elephant.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Elephant.java
@@ -1,9 +1,14 @@
 package de.hhn.it.devtools.components.chess2.pieces;
 
+import de.hhn.it.devtools.apis.chess2.Coordinate;
 import de.hhn.it.devtools.apis.chess2.Piece;
 
 public class Elephant extends Piece {
 
+  public Elephant(char color, Coordinate coordinate) {
+    this.color = color;
+    this.coordinate = coordinate;
+  }
   public void calculate() {
   }
 }
--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/King.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/King.java
@@ -1,5 +1,16 @@
 package de.hhn.it.devtools.components.chess2.pieces;
 
-public class King {
+import de.hhn.it.devtools.apis.chess2.Coordinate;
+import de.hhn.it.devtools.apis.chess2.Piece;
 
+public class King extends Piece {
+
+  public King(char color, Coordinate coordinate) {
+    this.color = color;
+    this.coordinate = coordinate;
+  }
+
+  @Override
+  protected void calculate() {
+  }
 }
