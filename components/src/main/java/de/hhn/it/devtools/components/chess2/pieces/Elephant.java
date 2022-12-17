fe26d8bab0a79fae5d4aa474be709bcd4a1ccd1d
Lara Mangi <crazyjasmin9@gmail.com>
2022-12-17 21:49:25 +0100

    doc: i write the calculation for the elephant, king and the queen.

--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Elephant.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Elephant.java
@@ -2,12 +2,36 @@ package de.hhn.it.devtools.components.chess2.pieces;
 
 import de.hhn.it.devtools.apis.chess2.Coordinate;
 import de.hhn.it.devtools.apis.chess2.Piece;
+import java.util.ArrayList;
 
 public class Elephant extends Piece {
 
   public Elephant(char color, Coordinate coordinate) {
     super(color, coordinate);
   }
+
   public void calculate() {
+    possibleMoves = new Coordinate[10];
+    int k = 0;
+
+    for (int i = coordinate.getX() - 2; i < coordinate.getX() + 2; i += 2) {
+      for (int j = coordinate.getY() - 2; j < coordinate.getY() + 2; j += 2) {
+        possibleMoves[k++] = new Coordinate(i, j);
+        // darf nicht hin zu coordinate.getX() & coordinate.getY()+1
+      }
+    }
+
+    ArrayList<Integer> index = new ArrayList<>();
+    for (int i = 0; i < possibleMoves.length; i++) {
+      if ((possibleMoves[i].getX() == coordinate.getX()
+          && possibleMoves[i].getY() == coordinate.getY())
+          || possibleMoves[i].getY() < 0     //steht der Bear am Rand
+          || possibleMoves[i].getX() < 0     //steht der Bear am Rand
+          || possibleMoves[i].getY() > 7     //steht der Bear am Rand
+          || possibleMoves[i].getX() > 7) {  //steht der Bear am Rand
+        index.add(i);
+      }
+    }
+    possibleMoves = shortenCoordinateArray(possibleMoves, index);
   }
 }
--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Queen.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Queen.java
@@ -2,6 +2,7 @@ package de.hhn.it.devtools.components.chess2.pieces;
 
 import de.hhn.it.devtools.apis.chess2.Coordinate;
 import de.hhn.it.devtools.apis.chess2.Piece;
+import java.util.ArrayList;
 
 public class Queen extends Piece {
 
@@ -11,6 +12,26 @@ public class Queen extends Piece {
 
   @Override
   protected void calculate() {
-
+    possibleMoves = new Coordinate[64];
+    int k = 0;
+    for (int g = 1; g <= 7; g++) {
+      for (int i = coordinate.getX() - g; i < coordinate.getX() + g; i += g) {
+        for (int j = coordinate.getY() - g; j < coordinate.getY() + g; j += g) {
+          possibleMoves[k++] = new Coordinate(i, j);
+        }
+      }
+    }
+    ArrayList<Integer> index = new ArrayList<>();
+    for (int i = 0; i < possibleMoves.length; i++) {
+      if ((possibleMoves[i].getX() == coordinate.getX()
+          && possibleMoves[i].getY() == coordinate.getY())
+          || possibleMoves[i].getY() < 0     //steht der Bear am Rand
+          || possibleMoves[i].getX() < 0     //steht der Bear am Rand
+          || possibleMoves[i].getY() > 7     //steht der Bear am Rand
+          || possibleMoves[i].getX() > 7) {  //steht der Bear am Rand
+        index.add(i);
+      }
+    }
+    possibleMoves = shortenCoordinateArray(possibleMoves, index);
   }
 }
