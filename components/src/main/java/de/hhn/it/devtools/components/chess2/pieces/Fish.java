92f875deb9a6f01319322f7a7f4c8a0eda79c78a
Lara Mangi <crazyjasmin9@gmail.com>
2022-12-17 20:29:25 +0100

    fix: write continue instead of null

--- components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Fish.java
+++ components/src/main/java/de/hhn/it/devtools/components/chess2/pieces/Fish.java
@@ -12,28 +12,27 @@ public class Fish extends Piece {
 
   @Override
   protected void calculate() {
-    possibleMoves = new Coordinate[7];
+    possibleMoves = new Coordinate[6];
     int k = 0;
     if (color == 'w') {
       for (int i = coordinate.getX() - 1; i < coordinate.getX() + 1; i++) {
         for (int j = coordinate.getY(); j < coordinate.getY() + 1; j++) {
           if ((i == coordinate.getX()) && (j == coordinate.getY() + 1)) {
-            possibleMoves[k++] = null;
-          } else {
-            possibleMoves[k++] = new Coordinate(i, j);
-            // darf nicht hin zu coordinate.getX() & coordinate.getY()+1
+            continue;
           }
+          possibleMoves[k++] = new Coordinate(i, j);
+          // darf nicht hin zu coordinate.getX() & coordinate.getY()+1
         }
       }
+
     } else if (color == 'b') {
       for (int i = coordinate.getX() - 1; i < coordinate.getX() + 1; i++) {
         for (int j = coordinate.getY() - 1; j < coordinate.getY() + 1; j++) {
           if ((i == coordinate.getX()) && (j == coordinate.getY() - 1)) {
-            possibleMoves[k++] = null;
-          } else {
-            possibleMoves[k++] = new Coordinate(i, j);
-            // darf nicht hin zu coordinate.getX() & coordinate.getY()-1
+            continue;
           }
+          possibleMoves[k++] = new Coordinate(i, j);
+          // darf nicht hin zu coordinate.getX() & coordinate.getY()-1
         }
       }
     }
