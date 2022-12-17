ba31d7456f17cca9a3b726df516461c353c757db
Tristan Nußbaum <gragonator02@gmail.com>
2022-12-17 23:14:38 +0100

    Implemented test for bad case

--- components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestStatBadCases.java
+++ components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/TestStatBadCases.java
@@ -1,60 +1,17 @@
 package de.hhn.it.devtools.components.ttrpgsheets.junit;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import de.hhn.it.devtools.components.ttrpgsheets.Stat;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TestStatBadCases {
+  private static final org.slf4j.Logger logger
+          = org.slf4j.LoggerFactory.getLogger(TestStatGoodCases.class);
 
   @Test
-  void getTotalValue() {
-  }
-
-  @Test
-  void addAbilityPoint() {
-  }
-
-  @Test
-  void removeAbilityPoint() {
-  }
-
-  @Test
-  void getType() {
-  }
-
-  @Test
-  void getBaseValue() {
-  }
-
-  @Test
-  void getAbilityPointsUsed() {
-  }
-
-  @Test
-  void setAbilityPointsUsed() {
-  }
-
-  @Test
-  void getOffset() {
-  }
-
-  @Test
-  void getMiscellaneous() {
-  }
-
-  @Test
-  void setMiscellaneous() {
-  }
-
-  @Test
-  void isLevelStat() {
-  }
-
-  @Test
-  void setLevelStat() {
-  }
-
-  @Test
-  void testToString() {
+  void constructorTest() {
+    logger.info("constructorTest() is called");
+    assertThrows(IllegalArgumentException.class, () -> new Stat(null));
   }
 }
