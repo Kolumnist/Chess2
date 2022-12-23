7e2fca597307e4c4f4c07deb16bf8a1b0bfbcde3
Maximilian König <maximilian.koenig@mein.gmx>
2022-12-23 14:13:47 +0100

    + CmpReactionGameService.java created

--- components/src/main/java/de/hhn/it/devtools/components/reactiongame/provider/Player.java
+++ components/src/main/java/de/hhn/it/devtools/components/reactiongame/provider/Player.java
@@ -2,12 +2,4 @@ package de.hhn.it.devtools.components.reactiongame.provider;
 
 public class Player {
 
-  /**
-   * The current amount of Player lifes.
-   *
-   * @param numberOfLifes Number of Player lifes.
-   */
-  void currentLife(int numberOfLifes) {
-
-  }
 }
