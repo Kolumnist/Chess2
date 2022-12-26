dcd792b8051ebf592d4832ecb6e48f37b5675210
Cspielmann <Cspielmann@stud.hs-heilbronn.de>
2022-12-26 17:01:00 +0100

    test: added UnitTest shouldReturnPanelLetter_d to proof thaht the wordlePanelD.getLetter() method returns that correct expected letter of the panel.

--- components/src/test/java/de/hhn/it/devtools/components/wordle/junit/TestWordlePanel.java
+++ components/src/test/java/de/hhn/it/devtools/components/wordle/junit/TestWordlePanel.java
@@ -1,4 +1,15 @@
 package de.hhn.it.devtools.components.wordle.junit;
 
+import de.hhn.it.devtools.components.wordle.provider.WordlePanel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestWordlePanel {
+
+    @Test
+    void shouldReturnPanelLetter_d() {
+        WordlePanel wordlePanelD = new WordlePanel('d', 0);
+        assertEquals('d', wordlePanelD.getLetter());
+    }
 }
