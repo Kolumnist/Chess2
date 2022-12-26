f9c8017ca3a450134a4b95897123cdabc1060e97
Patrick Franz <patrick.franz@franz-vs.de>
2022-12-26 14:18:23 +0100

    test: added TestWordleGuess.java for testing WordleGuess.java methods with UnitTests

---
+++ components/src/test/java/de/hhn/it/devtools/components/wordle/junit/TestWordleGuess.java
@@ -0,0 +1,39 @@
+package de.hhn.it.devtools.components.wordle.junit;
+
+import de.hhn.it.devtools.components.wordle.provider.WordleGuess;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestWordleGuess {
+
+  @Test
+  void shouldReturnString_whack() {
+    WordleGuess wordleGuess = new WordleGuess("whack");
+    assertEquals("whack", wordleGuess.getWordleGuessAsString());
+  }
+
+  @Test
+  void shouldReturnString_legal() {
+    WordleGuess wordleGuess = new WordleGuess("LeGaL");
+    assertEquals("legal", wordleGuess.getWordleGuessAsString());
+  }
+
+  @Test
+  void shouldReturnChar_b() {
+    WordleGuess wordleGuess = new WordleGuess("zebra");
+    assertEquals('b', wordleGuess.getLetterAtIndex(2));
+  }
+
+  @Test
+  void shouldReturnChar_e() {
+    WordleGuess wordleGuess = new WordleGuess("zEbra");
+    assertEquals('e', wordleGuess.getLetterAtIndex(1));
+  }
+
+  @Test
+  void setCharAtIndex3ToChar_g() {
+    WordleGuess wordleGuess = new WordleGuess("zebra");
+    wordleGuess.setLetterAtIndex(3,'g');
+    assertEquals('g', wordleGuess.getLetterAtIndex(3));
+  }
+}
