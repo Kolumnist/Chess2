44600196620a7a6dff0b71ac45e4edad92c93e95
Collin <96417832+Kolumnist@users.noreply.github.com>
2022-12-28 19:59:58 +0100

    feat: I wanted to test more today but I tested a bit and am almost done with the "easy good tests"

--- components/src/test/java/de/hhn/it/devtools/components/chess2/junit/TestChessGameGoodCases.java
+++ components/src/test/java/de/hhn/it/devtools/components/chess2/junit/TestChessGameGoodCases.java
@@ -3,10 +3,13 @@ package de.hhn.it.devtools.components.chess2.junit;
 import de.hhn.it.devtools.apis.chess2.Board;
 import de.hhn.it.devtools.apis.chess2.Coordinate;
 import de.hhn.it.devtools.apis.chess2.Chess2Service;
+import de.hhn.it.devtools.apis.chess2.Field;
 import de.hhn.it.devtools.apis.chess2.FieldState;
 import de.hhn.it.devtools.apis.chess2.GameState;
 import de.hhn.it.devtools.apis.chess2.IllegalStateException;
+import de.hhn.it.devtools.apis.chess2.InvalidMoveException;
 import de.hhn.it.devtools.apis.chess2.WinningPlayerState;
+import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
 import de.hhn.it.devtools.components.chess2.ChessGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Test ChessGame and Chess2Service interface with good cases.")
 public class TestChessGameGoodCases {
@@ -32,27 +36,111 @@ public class TestChessGameGoodCases {
   }
 
   @Test
-  @DisplayName("")
+  @DisplayName("Start a completely new Game and test all important variables")
   void TestStartGame() {
     Board board = chess2Service.startNewGame();
     assertNotNull(board);
-    Board board2 = chess2Service.startNewGame();
-    assertEquals(board, board2);
+    assertEquals(68, board.getFields().length);
+
+    int bearNum = 0;
+    int otherNum = 0;
+    int currentNum = 0;
+    int freeNum = 0;
+    for (Field field : board.getFields()) {
+      if (field.getFieldState() == FieldState.FREE_FIELD) {
+        freeNum++;
+        continue;
+      }
+      if (field.getFieldState() == FieldState.HAS_CURRENT_PIECE) {
+        assertEquals('w', field.getPiece().getColor());
+        currentNum++;
+        continue;
+      }
+      if (field.getFieldState() == FieldState.HAS_OTHER_PIECE) {
+        otherNum++;
+        continue;
+      }
+      if (field.getFieldState() == FieldState.HAS_BEAR) {
+        bearNum++;
+        assertTrue(field.getCoordinate().getX() > 0 && field.getCoordinate().getX() < 7
+            && field.getCoordinate().getY() > 2 && field.getCoordinate().getY() < 5);
+      }
+    }
+    assertEquals(1, bearNum);
+    assertEquals(16, otherNum);
+    assertEquals(16, currentNum);
+    assertEquals(31, freeNum);
   }
 
   @Test
-  @DisplayName("")
-  void TestEndGame() {
-    //TODO minimum 1
-    //chess2Service.endGame();
+  @DisplayName("Ending a Game of Chess which sets GameState to null")
+  void TestEndGame() throws IllegalStateException {
+    chess2Service.endGame();
+    assertNull(chess2Service.getGameState());
   }
 
   @Test
-  @DisplayName("")
-  void TestGiveUp() {
-    //TODO minimum 2
+  @DisplayName("startGame after endGame every variable is the same as in startGame")
+  void TestStartingGameBackUpAfterEndGame() throws IllegalStateException {
+    Board board = chess2Service.startNewGame();
+    assertNotNull(board);
+    chess2Service.endGame();
+    chess2Service.startNewGame();
+
+    assertEquals(GameState.RUNNING, chess2Service.getGameState());
+    assertNotNull(board);
+    assertEquals(68, board.getFields().length);
+
+    int bearNum = 0;
+    int otherNum = 0;
+    int currentNum = 0;
+    int freeNum = 0;
+    for (Field field : board.getFields()) {
+      if (field.getFieldState() == FieldState.FREE_FIELD) {
+        freeNum++;
+        continue;
+      }
+      if (field.getFieldState() == FieldState.HAS_CURRENT_PIECE) {
+        assertEquals('w', field.getPiece().getColor());
+        currentNum++;
+        continue;
+      }
+      if (field.getFieldState() == FieldState.HAS_OTHER_PIECE) {
+        otherNum++;
+        continue;
+      }
+      if (field.getFieldState() == FieldState.HAS_BEAR) {
+        bearNum++;
+        assertTrue(field.getCoordinate().getX() > 0 && field.getCoordinate().getX() < 7
+            && field.getCoordinate().getY() > 2 && field.getCoordinate().getY() < 5);
+      }
+    }
+    assertEquals(1, bearNum);
+    assertEquals(16, otherNum);
+    assertEquals(16, currentNum);
+    assertEquals(31, freeNum);
+  }
+
+  @Test
+  @DisplayName("Player White gives Up and Black wins")
+  void TestWhiteGiveUp() {
+    Board board = chess2Service.startNewGame();
     chess2Service.giveUp();
-    chess2Service.getWinningPlayer();
+    assertEquals(WinningPlayerState.BLACK_WIN, chess2Service.getWinningPlayer());
+  }
+
+  @Test
+  @DisplayName("Player Black gives Up and White wins")
+  void TestBlackGiveUp()
+      throws IllegalStateException, IllegalParameterException, InvalidMoveException {
+    Board board = chess2Service.startNewGame();
+
+    Coordinate[] currentCoords = chess2Service.getCurrentFields();
+    Coordinate[] possibleMoves = chess2Service.getPossibleMoves(currentCoords[0]);
+    chess2Service.moveSelectedPiece(currentCoords[0], new Coordinate(4, 1));
+
+    chess2Service.giveUp();
+    assertEquals(WinningPlayerState.WHITE_WIN, chess2Service.getWinningPlayer());
   }
 
   @Test
