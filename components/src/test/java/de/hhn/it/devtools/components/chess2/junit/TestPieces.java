1ae57992e6a173633b2f9362114ae365dc885524
Michel Jouaux <mjouaux@stud.hs-heilbronn.de>
2022-12-30 12:54:10 +0100

    Feat: Created the class Test Pieces

---
+++ components/src/test/java/de/hhn/it/devtools/components/chess2/junit/TestPieces.java
@@ -0,0 +1,16 @@
+package de.hhn.it.devtools.components.chess2.junit;
+
+import de.hhn.it.devtools.apis.chess2.Coordinate;
+import de.hhn.it.devtools.apis.chess2.Piece;
+import de.hhn.it.devtools.components.chess2.ChessGame;
+import de.hhn.it.devtools.components.chess2.pieces.Bear;
+import de.hhn.it.devtools.components.chess2.pieces.Crow;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestPieces {
+
+  @BeforeEach
+  public void setUp(){
+    ChessGame chessGame = new ChessGame();
+  }
+}
