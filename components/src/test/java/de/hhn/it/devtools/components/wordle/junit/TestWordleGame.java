506b853c90e3bd378427ef462e90d3695943c9b6
MDrautz <madrautz@stud.hs-heilbronn.de>
2022-12-30 14:16:18 +0100

    test: established class TestWordleGame to unit test WordleGame

---
+++ components/src/test/java/de/hhn/it/devtools/components/wordle/junit/TestWordleGame.java
@@ -0,0 +1,19 @@
+package de.hhn.it.devtools.components.wordle.junit;
+
+import de.hhn.it.devtools.components.wordle.provider.WordleGame;
+import de.hhn.it.devtools.components.wordle.provider.WordleGameLogic;
+import de.hhn.it.devtools.components.wordle.provider.WordleGuess;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+
+public class TestWordleGame {
+
+  @Test
+  void testWordleGameConstructor() {
+
+  }
+
+}
