c15a806132c439bd5020101a7d3cf2f32049c152
MDrautz <madrautz@stud.hs-heilbronn.de>
2022-12-30 13:42:52 +0100

    fix: added a WordleGameLogic as instance variable so that WordleGame can call the various methods from WordleGameLogic in order to proceed

--- components/src/main/java/de/hhn/it/devtools/components/wordle/provider/WordleGame.java
+++ components/src/main/java/de/hhn/it/devtools/components/wordle/provider/WordleGame.java
@@ -6,14 +6,18 @@ public class WordleGame {
   private WordleGuess[] playerGuesses;
   private int wordleGuessIndex;
   private int wordlePanelIndex;
+  private WordleGameLogic backend;
 
-  public WordleGame(String solution) {
+  public WordleGame(String solution, WordleGameLogic backend) {
     this.solution = solution;
     this.playerGuesses = new WordleGuess[6];
     this.wordleGuessIndex = 0;
     this.wordlePanelIndex = 0;
+    this.backend = backend;
     for (int i = 0; i <= 5; i++){
       playerGuesses[i] = new WordleGuess("     ");
     }
   }
+
+
 }
