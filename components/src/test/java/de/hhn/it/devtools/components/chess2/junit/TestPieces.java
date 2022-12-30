c9624b076f353d8ddeb44614cea0cd526b7d2124
JxWorker <micheljouaux@gmx.de>
2022-12-30 22:20:02 +0100

    Feat: Added test for the Bear

--- components/src/test/java/de/hhn/it/devtools/components/chess2/junit/TestPieces.java
+++ components/src/test/java/de/hhn/it/devtools/components/chess2/junit/TestPieces.java
@@ -6,6 +6,7 @@ import de.hhn.it.devtools.apis.chess2.Coordinate;
 import de.hhn.it.devtools.apis.chess2.Piece;
 import de.hhn.it.devtools.components.chess2.ChessGame;
 import de.hhn.it.devtools.components.chess2.pieces.Bear;
+import de.hhn.it.devtools.components.chess2.pieces.Crow;
 import de.hhn.it.devtools.components.chess2.pieces.King;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,7 +29,7 @@ public class TestPieces {
 
   //region King
   @Test
-  @DisplayName("Test if calculate produces the right coordinates if the king stand on the field 4, 4")
+  @DisplayName("Test if calculate produces the right coordinates if the king stands on the field 4, 4")
   public void testCalculateKingInTheMiddleOfTheBoard(){
     Piece king = new King('b',new Coordinate(4,4), true);
     Coordinate[] expected = {new Coordinate(3,3),new Coordinate(3,4),
@@ -42,7 +43,7 @@ public class TestPieces {
   }
 
   @Test
-  @DisplayName("Test if calculate produces the right coordinates if the king stand on the field 0, 6")
+  @DisplayName("Test if calculate produces the right coordinates if the king stands on the field 0, 6")
   public void testCalculateKingOnTheSideOfTheBoard(){
     Piece king = new King('b',new Coordinate(0,6), true);
     Coordinate[] expected = {new Coordinate(0,5),new Coordinate(0,7),
@@ -55,7 +56,7 @@ public class TestPieces {
   }
 
   @Test
-  @DisplayName("Test if calculate produces the right coordinates if the king stand on the field 7, 7")
+  @DisplayName("Test if calculate produces the right coordinates if the king stands on the field 7, 7")
   public void testCalculateKingInTheCornerOfTheBoard(){
     Piece king = new King('b',new Coordinate(7,7), true);
     Coordinate[] expected = {new Coordinate(6,6),new Coordinate(6,7),
@@ -68,7 +69,7 @@ public class TestPieces {
   }
 
   @Test
-  @DisplayName("Test if calculate produces the right coordinates if the king stand on an invalid field")
+  @DisplayName("Test if calculate produces the right coordinates if the king stands on an invalid field")
   public void testCalculateKingOnInvalidField(){
     Piece king = new King('b',new Coordinate(-5,9), true);
     king.calculate(board);
@@ -104,5 +105,102 @@ public class TestPieces {
       }
     }
   }
+
+  @Test
+  @DisplayName("Test if defeatPieceMove produces the right coordinates if the crow stands on the field 4, 4")
+  public void testDefeatPieceMoveInTheMiddleOfTheBoard(){
+    Crow crow = new Crow('b', new Coordinate(4,4));
+    crow.defeatPieceMove();
+
+    Coordinate[] expected = {new Coordinate(3,3),new Coordinate(3,4),
+            new Coordinate(3,5),new Coordinate(4,3),new Coordinate(4,5),
+            new Coordinate(5,3),new Coordinate(5,4),new Coordinate(5,5)};
+
+    for (int i = 0; i < crow.getPossibleMove().length; i++) {
+      assertEquals(expected[i].getX(),crow.getPossibleMove()[i].getX());
+      assertEquals(expected[i].getY(),crow.getPossibleMove()[i].getY());
+    }
+  }
+
+  @Test
+  @DisplayName("Test if defeatPieceMove produces the right coordinates if the crow stands on the field 0, 0")
+  public void testDefeatPieceMoveInTheCornerOfTheBoard(){
+    Crow crow = new Crow('b', new Coordinate(0,0));
+    crow.defeatPieceMove();
+
+    Coordinate[] expected = {new Coordinate(3,3),new Coordinate(3,4),
+            new Coordinate(3,5),new Coordinate(4,3),new Coordinate(4,5),
+            new Coordinate(5,3),new Coordinate(5,4),new Coordinate(5,5)};
+
+    for (int i = 0; i < crow.getPossibleMove().length; i++) {
+      assertEquals(expected[i].getX(),crow.getPossibleMove()[i].getX());
+      assertEquals(expected[i].getY(),crow.getPossibleMove()[i].getY());
+    }
+  }
+
+  //TODO: test Crow after it got moeved and a piece defeted
+
+  //endregion
+
+  //region Bear
+  @Test
+  @DisplayName("Test if calculate produces the right coordinates if the bear stands on the field 4, 4")
+  public void testCalculateBearInTheMiddleOfTheBoard(){
+    Piece bear = new Bear('g',new Coordinate(4,4));
+    bear.calculate(board);
+
+    Coordinate[] expected = {new Coordinate(3,3),new Coordinate(3,4),
+            new Coordinate(3,5),new Coordinate(4,3),new Coordinate(4,5),
+            new Coordinate(5,3),new Coordinate(5,4),new Coordinate(5,5)};
+
+    for (int i = 0; i < bear.getPossibleMove().length; i++) {
+      assertEquals(expected[i].getX(),bear.getPossibleMove()[i].getX());
+      assertEquals(expected[i].getY(),bear.getPossibleMove()[i].getY());
+    }
+  }
+
+  @Test
+  @DisplayName("Test if calculate produces the right coordinates if the bear stands on the field 0, 4")
+  public void testCalculateBearOnTheSideOfTheBoard(){
+    Piece bear = new Bear('g',new Coordinate(0,4));
+    bear.calculate(board);
+
+    Coordinate[] expected = {new Coordinate(0,3),new Coordinate(0,5),
+            new Coordinate(1,3),new Coordinate(1,4),new Coordinate(1,5)};
+
+    for (int i = 0; i < bear.getPossibleMove().length; i++) {
+      assertEquals(expected[i].getX(),bear.getPossibleMove()[i].getX());
+      assertEquals(expected[i].getY(),bear.getPossibleMove()[i].getY());
+    }
+  }
+
+  @Test
+  @DisplayName("Test if calculate produces the right coordinates if the bear stands near other pieces and on the field 4, 5")
+  public void testCalculateBearNearOtherPieces(){
+    Piece bear = new Bear('g',new Coordinate(4,5));
+    bear.calculate(board);
+
+    Coordinate[] expected = {new Coordinate(3,4),new Coordinate(3,5),
+            new Coordinate(4,4),new Coordinate(5,4),new Coordinate(5,5)};
+
+    for (int i = 0; i < bear.getPossibleMove().length; i++) {
+      assertEquals(expected[i].getX(),bear.getPossibleMove()[i].getX());
+      assertEquals(expected[i].getY(),bear.getPossibleMove()[i].getY());
+    }
+  }
+
+  @Test
+  @DisplayName("Test if calculate produces the right coordinates if the bear stands on an invalid field")
+  public void testCalculateBearOnInvalidField(){
+    Piece bear = new Bear('g',new Coordinate(-5,9));
+    bear.calculate(board);
+
+    for (int i = 0; i < bear.getPossibleMove().length; i++) {
+      assertEquals(null,bear.getPossibleMove()[i]);
+    }
+  }
+  //endregion
+
+  //region Monkey
   //endregion
 }
