67744bf1f73d99782451255b6e15bbdfcb8b9ab8
Jan <janseel03@gmail.com>
2023-01-04 16:41:12 +0100

    code clean up in Room.java

--- components/src/main/java/de/hhn/it/devtools/components/textBasedLabyrinth/textbasedlabyrinth/Room.java
+++ components/src/main/java/de/hhn/it/devtools/components/textBasedLabyrinth/textbasedlabyrinth/Room.java
@@ -18,28 +18,29 @@ public class Room {
 
   protected HashMap<Integer, Item> items;
   private HashMap<Direction, Room> roomMap;
+  private HashMap<Direction, Door> doorMap;
   protected int roomId;
 
-  private String description;
+  private final String description;
 
-  protected Room toTheNorth;
-  protected Room toTheSouth;
-  protected Room toTheEast;
-  protected Room toTheWest;
+  // protected Room toTheNorth;
+  // protected Room toTheSouth;
+  // protected Room toTheEast;
+  // protected Room toTheWest;
 
-  protected Boolean isNorthAssigned;
-  protected Boolean isSouthAssigned;
-  protected Boolean isEastAssigned;
-  protected Boolean isWestAssigned;
+  // protected Boolean isNorthAssigned;
+  // protected Boolean isSouthAssigned;
+  // protected Boolean isEastAssigned;
+  // protected Boolean isWestAssigned;
 
-  protected Door westDoor;
-  protected Door eastDoor;
-  protected Door northDoor;
-  protected Door southDoor;
-  private boolean hasDoorN;
-  private boolean hasDoorW;
-  private boolean hasDoorE;
-  private boolean hasDoorS;
+  // protected Door westDoor;
+  // protected Door eastDoor;
+  // protected Door northDoor;
+  // protected Door southDoor;
+  // private boolean hasDoorN;
+  // private boolean hasDoorW;
+  // private boolean hasDoorE;
+  // private boolean hasDoorS;
 
   private boolean setFakeNorth;
   private boolean setFakeSouth;
@@ -56,49 +57,45 @@ public class Room {
    */
   public Room(int id, String description) {
     this.roomId = id;
-    this.isNorthAssigned = false;
-    this.isSouthAssigned = false;
-    this.isEastAssigned = false;
-    this.isWestAssigned = false;
-    this.hasDoorS = false;
-    this.hasDoorE = false;
-    this.hasDoorN = false;
-    this.hasDoorW = false;
+    // this.hasDoorS = false;
+    // this.hasDoorE = false;
+    // this.hasDoorN = false;
+    // this.hasDoorW = false;
 
     items = new HashMap<>();
+    roomMap = new HashMap<>();
+    doorMap = new HashMap<>();
     this.description = description;
   }
 
   public void addItem(Item item) {
     items.put(item.getItemId(), item);
-
   }
 
   public Room getRoom(Direction direction) throws RoomFailedException {
     if(!roomMap.containsKey(direction)) {
       throw new RoomFailedException("No Room in this direction" + direction);
     }
-
-    Room room = null;
-    if(direction.equals(Direction.SOUTH)){
-      room = toTheSouth;
-    }
-    else if(direction.equals(Direction.NORTH)){
-      room = toTheNorth;
-    }
-    else if(direction.equals(Direction.EAST)){
-      room = toTheEast;
-    }
-    else{
-      room = toTheSouth;
-    }
-    return room;
+    return roomMap.get(direction);
   }
 
   /**
    * Gets the doors for the new room.
-   */
+   *
   public void setDoors() {
+
+    if(roomMap.containsKey(Direction.SOUTH)){
+      if(roomMap.get(Direction.SOUTH).doorMap.containsKey(Direction.SOUTH)){
+        southDoor = roomMap.get(Direction.SOUTH).doorMap.get(Direction.SOUTH)
+      }
+      else {
+        southDoor = new Door();
+      }
+    }
+
+
+
+
 
     if (isSouthAssigned) {
       if (toTheSouth.hasSouthDoor()) {
@@ -133,45 +130,26 @@ public class Room {
       hasDoorE = true;
     }
 
-  }
+  }*/
 
 
   protected void setFakeInDirection(Direction direction) throws IllegalArgumentException {
-    if (direction.equals(Direction.SOUTH) && !isSouthAssigned) {
-      southDoor = new Door();
-      isSouthAssigned = true;
-      southDoor.isFake();
-    } else if (direction.equals(Direction.NORTH) && !isNorthAssigned) {
-      northDoor = new Door();
-      isNorthAssigned = true;
-      northDoor.isFake();
-    } else if (direction.equals(Direction.EAST) && !isEastAssigned) {
-      eastDoor = new Door();
-      isEastAssigned = true;
-      eastDoor.isFake();
-    } else if (direction.equals(Direction.WEST) && !isWestAssigned) {
-      westDoor = new Door();
-      isWestAssigned = true;
-      westDoor.isFake();
-    } else {
+    if(!roomMap.containsKey(direction)){
+      doorMap.put(direction, new Door());
+      doorMap.get(direction).isFake();
+    }
+    else {
       throw new IllegalArgumentException("Direction was invalid for a fake door: " + direction.toString());
     }
   }
 
   public Door getDoor(Direction direction) throws RoomFailedException {
-    if (direction.equals(Direction.SOUTH) && isSouthAssigned) {
-      return southDoor;
-    } else if (direction.equals(Direction.NORTH) && isNorthAssigned) {
-      return northDoor;
-    } else if (direction.equals(Direction.EAST) && isEastAssigned) {
-      return eastDoor;
-    } else if (direction.equals(Direction.WEST) && isWestAssigned) {
-      return westDoor;
-    } else {
+    if (!doorMap.containsKey(direction)) {
       throw new RoomFailedException("No door found in this direction: " + direction.toString());
     }
-
-
+    else {
+      return doorMap.get(direction);
+    }
   }
 
   public void removeItem(int itemId) {
@@ -186,43 +164,12 @@ public class Room {
     return description;
   }
 
-  public Door getWestDoor() {
-    return westDoor;
-  }
-
-  public Door getEastDoor() {
-    return eastDoor;
-  }
-
-  public Door getNorthDoor() {
-    return northDoor;
-  }
-
-  public Door getSouthDoor() {
-    return southDoor;
-  }
-
-  public boolean hasSouthDoor() {
-    return hasDoorS;
-  }
-
-  public boolean hasDoorNorth() {
-    return hasDoorN;
-  }
-
-  public boolean hasDoorWest() {
-    return hasDoorW;
-  }
-
-  public boolean hasDoorEast() {
-    return hasDoorE;
-  }
 
   /**
    * Get number of doors.
    *
    * @return number of doors.
-   */
+   *
   public int getNumberOfDoors() {
     int a = 0;
     if (hasDoorS) {
@@ -238,9 +185,7 @@ public class Room {
       a++;
     }
     return a;
-  }
-
-
+  }*/
 
   /**
    * Searching for an item in the room.
@@ -260,26 +205,41 @@ public class Room {
    * @param room room to be assigned as the next door room
    */
   public void setNextDoorRoom(Room room,Direction direction) {
-    if (direction.equals(Direction.EAST)) {
-      this.toTheEast = room;
-      this.isEastAssigned = true;
-      room.toTheWest = this;
-      room.isWestAssigned = true;
-    } else if (direction.equals(Direction.WEST)) {
-      this.toTheWest = room;
-      this.isWestAssigned = true;
-      room.toTheWest = this;
-      room.isEastAssigned = true;
-    } else if (direction.equals(Direction.NORTH)) {
-      this.toTheNorth = room;
-      this.isNorthAssigned = true;
-      room.toTheSouth = this;
-      room.isSouthAssigned = true;
-    } else if (direction.equals(Direction.SOUTH)) {
-      this.toTheSouth = room;
-      this.isSouthAssigned = true;
-      room.toTheNorth = this;
-      room.isNorthAssigned = true;
+    if(roomMap.containsKey(direction)){
+      if(direction.equals(Direction.SOUTH)){
+        roomMap.replace(direction, room);
+        roomMap.get(direction).roomMap.replace(Direction.NORTH, this);
+      }
+      else if(direction.equals(Direction.NORTH)) {
+        roomMap.replace(direction, room);
+        roomMap.get(direction).roomMap.replace(Direction.SOUTH, this);
+      }
+      else if(direction.equals(Direction.EAST)) {
+        roomMap.replace(direction, room);
+        roomMap.get(direction).roomMap.replace(Direction.WEST, this);
+      }
+      else {
+        roomMap.replace(direction, room);
+        roomMap.get(direction).roomMap.replace(Direction.EAST, this);
+      }
+    }
+    else {
+      if(direction.equals(Direction.SOUTH)){
+        roomMap.put(direction, room);
+        roomMap.get(direction).roomMap.put(Direction.NORTH, this);
+      }
+      else if(direction.equals(Direction.NORTH)) {
+        roomMap.put(direction, room);
+        roomMap.get(direction).roomMap.put(Direction.SOUTH, this);
+      }
+      else if(direction.equals(Direction.EAST)) {
+        roomMap.put(direction, room);
+        roomMap.get(direction).roomMap.put(Direction.WEST, this);
+      }
+      else {
+        roomMap.put(direction, room);
+        roomMap.get(direction).roomMap.put(Direction.EAST, this);
+      }
     }
   }
 
@@ -288,3 +248,60 @@ public class Room {
   }
 
 }
+
+/**
+ * if (direction.equals(Direction.EAST)) {
+ *       this.toTheEast = room;
+ *       this.isEastAssigned = true;
+ *       room.toTheWest = this;
+ *       room.isWestAssigned = true;
+ *     } else if (direction.equals(Direction.WEST)) {
+ *       this.toTheWest = room;
+ *       this.isWestAssigned = true;
+ *       room.toTheWest = this;
+ *       room.isEastAssigned = true;
+ *     } else if (direction.equals(Direction.NORTH)) {
+ *       this.toTheNorth = room;
+ *       this.isNorthAssigned = true;
+ *       room.toTheSouth = this;
+ *       room.isSouthAssigned = true;
+ *     } else if (direction.equals(Direction.SOUTH)) {
+ *       this.toTheSouth = room;
+ *       this.isSouthAssigned = true;
+ *       room.toTheNorth = this;
+ *       room.isNorthAssigned = true;
+ *     }
+ *
+ *     if (direction.equals(Direction.SOUTH) && !isSouthAssigned) {
+ *       southDoor = new Door();
+ *       isSouthAssigned = true;
+ *       southDoor.isFake();
+ *     } else if (direction.equals(Direction.NORTH) && !isNorthAssigned) {
+ *       northDoor = new Door();
+ *       isNorthAssigned = true;
+ *       northDoor.isFake();
+ *     } else if (direction.equals(Direction.EAST) && !isEastAssigned) {
+ *       eastDoor = new Door();
+ *       isEastAssigned = true;
+ *       eastDoor.isFake();
+ *     } else if (direction.equals(Direction.WEST) && !isWestAssigned) {
+ *       westDoor = new Door();
+ *       isWestAssigned = true;
+ *       westDoor.isFake();
+ *
+ *       public boolean hasSouthDoor() {
+ *     return hasDoorS;
+ *   }
+ *
+ *   public boolean hasDoorNorth() {
+ *     return hasDoorN;
+ *   }
+ *
+ *   public boolean hasDoorWest() {
+ *     return hasDoorW;
+ *   }
+ *
+ *   public boolean hasDoorEast() {
+ *     return hasDoorE;
+ *   }
+ */
