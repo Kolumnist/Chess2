9f5f2b9a18ad588b3263509cc193acf97a60f982
Tristan Nußbaum <gragonator02@gmail.com>
2023-01-06 18:32:09 +0100

    Removed * import

--- components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/StatTest.java
+++ components/src/test/java/de/hhn/it/devtools/components/ttrpgsheets/junit/StatTest.java
@@ -1,6 +1,8 @@
 package de.hhn.it.devtools.components.ttrpgsheets.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import de.hhn.it.devtools.apis.ttrpgsheets.StatDescriptor;
 import de.hhn.it.devtools.apis.ttrpgsheets.StatType;
