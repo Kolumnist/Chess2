83b58ef41643783ac0107d28a9af06253d84db3a
Hannes Jetter <hannes.jetter@googlemail.com>
2023-01-07 22:08:31 +0100

    added ServiceUsageDemo

---
+++ components/src/test/java/de/hhn/it/devtools/components/duckhunt/DuckHuntServiceUsageDemo.java
@@ -0,0 +1,54 @@
+package de.hhn.it.devtools.components.duckhunt;
+
+import de.hhn.it.devtools.apis.duckhunt.*;
+import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
+
+public class DuckHuntServiceUsageDemo implements DuckHuntListener {
+    private static final org.slf4j.Logger logger =
+            org.slf4j.LoggerFactory.getLogger(DuckHuntServiceUsageDemo.class);
+    public DucksInfo ducksInfo;
+
+    public static void main(String[] args) throws IllegalParameterException, InterruptedException {
+        // main menu
+
+        // create new game with default settings
+        DuckHunt game = new DuckHunt();
+
+        // add listener
+        DuckHuntServiceUsageDemo listener = new DuckHuntServiceUsageDemo();
+        game.addCallback(listener);
+
+        game.startGame();
+
+        Thread.sleep(5000);
+        game.pauseGame();
+        Thread.sleep(1000);
+
+        // shoot at duck
+        game.shoot(listener.ducksInfo.duckData()[0].getX(), listener.ducksInfo.duckData()[0].getY());
+
+        Thread.sleep(1000);
+        //game.pauseGame();
+
+        System.out.println("Test");
+        game.stopGame();
+    }
+
+    @Override
+    public void newState(GameInfo gameInfo) throws IllegalGameInfoException {
+        logger.debug(gameInfo.toString());
+    }
+
+    @Override
+    public void newDuckPosition(DucksInfo duckPosition) throws IllegalDuckPositionException {
+        ducksInfo = duckPosition;
+        for (DuckData duck : duckPosition.duckData()) {
+            logger.debug(duck.toString());
+        }
+    }
+
+    @Override
+    public void duckHit(int id) throws IllegalDuckIdException {
+        logger.debug("Duck " + id + " was hit");
+    }
+}
