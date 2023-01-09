1658509fba9ae3157ca0f165fd91f2aa961f447e
shira <sschaefer@stud.hs-heilbronn.de>
2023-01-09 11:05:50 +0100

    added fmxl and controller gamefield and card

---
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/memory/CardController.java
@@ -0,0 +1,17 @@
+package de.hhn.it.devtools.javafx.controllers.memory;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CardController {
+
+    @FXML
+    private Button cardButton;
+
+    @FXML
+    void onCardButtonClicked(ActionEvent event) {
+
+    }
+
+}
--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/memory/MemoryGameController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/memory/MemoryGameController.java
@@ -1,4 +1,36 @@
 package de.hhn.it.devtools.javafx.controllers.memory;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class MemoryGameController {
+
+    @FXML
+    private Button finishButton;
+
+    @FXML
+    private AnchorPane gameScreenAnchorPane;
+
+    @FXML
+    private Button optionsButton;
+
+    @FXML
+    private Label timeDisplayLabel;
+
+    @FXML
+    private Label timeTextLabel;
+
+    @FXML
+    void onFinishButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOptionsButtonClicked(ActionEvent event) {
+
+    }
+
 }
