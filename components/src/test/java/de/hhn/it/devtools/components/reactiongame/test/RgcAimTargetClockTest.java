a3fda40abbd1ee4cc447fd8a4e7ed6762f0e6d5c
Tobias Bergtold <tbergtold@stud.hs-heilbronn.de>
2023-01-13 14:45:43 +0100

    + Added test class for RgcAimTarget + Added test class for RgcAimTargetClock

---
+++ components/src/test/java/de/hhn/it/devtools/components/reactiongame/test/RgcAimTargetClockTest.java
@@ -0,0 +1,20 @@
+package de.hhn.it.devtools.components.reactiongame.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RgcAimTargetClockTest {
+
+  @Test
+  void setRunning() {
+  }
+
+  @Test
+  void setEnded() {
+  }
+
+  @Test
+  void run() {
+  }
+}
---
+++ components/src/test/java/de/hhn/it/devtools/components/reactiongame/test/RgcAimTargetTest.java
@@ -0,0 +1,32 @@
+package de.hhn.it.devtools.components.reactiongame.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RgcAimTargetTest {
+
+  @Test
+  void getX() {
+  }
+
+  @Test
+  void getY() {
+  }
+
+  @Test
+  void getR() {
+  }
+
+  @Test
+  void getKey() {
+  }
+
+  @Test
+  void getId() {
+  }
+
+  @Test
+  void testToString() {
+  }
+}
