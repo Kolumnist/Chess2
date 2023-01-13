f13e95d6358334c70c030f3c8baec7342ef1bd3a
Tobias Bergtold <tbergtold@stud.hs-heilbronn.de>
2023-01-13 16:19:20 +0100

    + Added tests for RgcAimTargetTest

--- components/src/test/java/de/hhn/it/devtools/components/reactiongame/test/RgcAimTargetTest.java
+++ components/src/test/java/de/hhn/it/devtools/components/reactiongame/test/RgcAimTargetTest.java
@@ -1,32 +1,51 @@
 package de.hhn.it.devtools.components.reactiongame.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
+import de.hhn.it.devtools.components.reactiongame.provider.RgcAimTarget;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RgcAimTargetTest {
 
+  private RgcAimTarget target;
+  @BeforeEach
+  void setup(){
+    target = new RgcAimTarget(1, 12, 15, 25, 'q');
+  }
   @Test
-  void getX() {
+  void testGetX() {
+    assertTrue(target.getX() == 12);
   }
 
   @Test
-  void getY() {
+  void testGetY() {
+    assertTrue(target.getY() == 15);
   }
 
   @Test
-  void getR() {
+  void testGetR() {
+    assertTrue(target.getR() == 25);
   }
 
   @Test
-  void getKey() {
+  void testGetKey() {
+    assertEquals('q', target.getKey());
   }
 
   @Test
-  void getId() {
+  void testGetId() {
+    assertTrue(target.getId() == 1);
   }
 
   @Test
   void testToString() {
+    assertEquals("AimTarget{"
+        + "x=" + 12
+        + ", y=" + 15
+        + ", r=" + 25
+        + ", key=" + 'q'
+        + ", id=" + 1
+        + '}' , target.toString());
   }
 }
