3df1a48132c2a41f5053d203755a8cf5594a7b2f
Tobias Bergtold <tbergtold@stud.hs-heilbronn.de>
2023-01-13 23:54:08 +0100

    + Added RgcRunTest2

---
+++ components/src/test/java/de/hhn/it/devtools/components/reactiongame/test/RgcRunTest2.java
@@ -0,0 +1,43 @@
+package de.hhn.it.devtools.components.reactiongame.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import de.hhn.it.devtools.apis.reactiongame.Difficulty;
+import de.hhn.it.devtools.components.reactiongame.provider.RgcPlayer;
+import de.hhn.it.devtools.components.reactiongame.provider.RgcRun;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RgcRunTest2 {
+  RgcRun run;
+
+  @BeforeEach
+  void setUp() {
+    run = new RgcRun(Difficulty.MEDIUM, new RgcPlayer("Player"));
+  }
+  @Test
+  void testAddAndRemoveObstacle() {
+
+    run.addObstacle(0);
+
+    assertNotEquals(0, run.getField().getObstacles().size());
+
+    run.removeObstacle(0);
+
+    assertEquals(0, run.getField().getObstacles().size());
+  }
+
+  @Test
+  void testAddAndRemoveAimTarget() {
+
+    run.addAimTarget(0);
+
+    assertNotEquals(0, run.getField().getTargets().size());
+
+    run.removeAimTarget(0);
+
+    assertEquals(0, run.getField().getTargets().size());
+  }
+
+}
