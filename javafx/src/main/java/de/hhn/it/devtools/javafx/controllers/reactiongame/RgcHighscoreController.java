f8da6dc5b59ce24f2e532e9db88aa98662620740
Jonas Gierlich <jgierlich@stud.hs-heilbronn.de>
2023-01-14 18:14:56 +0100

    added more details to the highscoreController and the highscore file

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/reactiongame/RgcHighscoreController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/reactiongame/RgcHighscoreController.java
@@ -8,20 +8,52 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class RgcHighscoreController implements Initializable {
 
   private RgcScreenController screenController;
+
+  @FXML
+  private Label highscore1;
+
+  @FXML
+  private Label highscore2;
+
+  @FXML
+  private Label highscore3;
+
+  @FXML
+  private Label highscore4;
+
+  @FXML
+  private Label highscore5;
+
+  @FXML
+  private Label highscore6;
+
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     SingletonAttributeStore singletonAttributeStore = SingletonAttributeStore.getReference();
     screenController =
         (RgcScreenController) singletonAttributeStore.getAttribute(ReactionGameController.RGC_SCREEN_CONTROLLER);
+
+    readHighscores();
+  }
+
+  private void readHighscores() {
+
+  }
+
+  private void writeHighscores() {
+
   }
 
   @FXML
   void onBackToMenuBtn(ActionEvent event) {
+    writeHighscores();
+
     try {
       screenController.switchTo("RgcMenu");
     } catch (IOException e) {
