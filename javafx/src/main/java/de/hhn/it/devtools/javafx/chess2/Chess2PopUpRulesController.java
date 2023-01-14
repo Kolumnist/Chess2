b3dd5217874838ebed41bd08cdb54cbbc774fd68
Lara Mangi <crazyjasmin9@gmail.com>
2023-01-14 12:54:08 +0100

    i correct a mistake in the chess2popupruler class

--- javafx/src/main/java/de/hhn/it/devtools/javafx/chess2/Chess2PopUpRulesController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/chess2/Chess2PopUpRulesController.java
@@ -29,17 +29,17 @@ public class Chess2PopUpRulesController {
 
       new Rules("Fish",
           "He can walk 1 field to the right, legft or diagonally forward. He can only\n "
-              + "defeat enemies forward diagonal.\bThe fish becomes a fishqueen by reaching the other end of the board.\n"),
+              + "defeat enemies forward diagonal.\bThe fish becomes a fishqueen by reaching the other end of the board.\n\n"),
       new Rules("Fishqueen",
-          "Its the same as the queen but doesn't go to jail.\n"),
+          "Its the same as the queen but doesn't go to jail.\n\n"),
       new Rules("Elephant",
           "He moves 2 fields only and defeats enemies standing on the second field.\n "
-              + "He can move in every direction.\n"),
+              + "He can move in every direction.\n\n"),
       new Rules("Bear",
           "Sits in the middle of the chessboard. It can move 1 field in every direction\n "
               + "and both players can move him on their turns. It can defeated by both.\bIt is a\n "
               + "friendly bear and can't defeat anyone on his own but if he gets defeated the piece\n "
-              + "which defeated him gets defeated as well.\n"),
+              + "which defeated him gets defeated as well.\n\n"),
       new Rules("Monkey",
           "He can move 1 field in every direction and jumps over other pieces once, after\n "
               + "jumping he can jump again if there is another piece which he hasn't jumped over yet.\n "
@@ -47,32 +47,32 @@ public class Chess2PopUpRulesController {
               + "is in jail and still has his banana, the monkey is able to free the King if another piece\n "
               + "is positioned so that the monkey lands on the field next to the king after jumping over the\n "
               + "positioned piece. The monkey frees the king, takes the banana and lands back to the field before\n "
-              + "he was jumping over the positioned piece and the king lands next to the jail where the monkey freed him.\n"),
+              + "he was jumping over the positioned piece and the king lands next to the jail where the monkey freed him.\n\n"),
       new Rules("Crow",
           "It can move to any field which is not occupied. It can only defeat an enemy in\n "
-              + "field range when the opposing player defeated any piece on their turn\n"),
+              + "field range when the opposing player defeated any piece on their turn\n\n"),
       new Rules("King",
           "He can do the same as before but he has a banana. If he gets defeated once he\n "
               + "goes to jail with his banana.\bThe monkey can then free him but he will lose the\n "
-              + "banana and won't get out of jail anymore.\n"),
+              + "banana and won't get out of jail anymore.\n\n"),
       new Rules("Queen",
           "Queen is still the queen but also has to be in jail to win the game, upon\n "
-              + "defeating she goes to jail. The queen can not get free from jail\n"),
+              + "defeating she goes to jail. The queen can not get free from jail\n\n"),
       new Rules("Jail",
           "It is on the right and left side of the board and is an extension to it!\n "
-              + "The defeated king and/or queen is imprisoned, only those two go to jail upon defeat.\n"));
+              + "The defeated king and/or queen is imprisoned, only those two go to jail upon defeat.\n\n"));
 
 
   @FXML
   public void initialize() {
     Stage dialog = new Stage();
-    columnFigur.setCellValueFactory(new PropertyValueFactory<Rules, String>("Figures"));
+    columnFigur.setCellValueFactory(new PropertyValueFactory<Rules, String>("Figur"));
     columnDescription.setCellValueFactory(new PropertyValueFactory<Rules, String>("Description"));
     myTableView.setFixedCellSize(Region.USE_COMPUTED_SIZE);
     myTableView.setItems(list);
-    VBox dialogVbox = new VBox(50);
+    VBox dialogVbox = new VBox(10);
     dialogVbox.getChildren().add(myTableView);
-    Scene dialogScene = new Scene(dialogVbox, 1000, 1000);
+    Scene dialogScene = new Scene(dialogVbox, 600, 400);
     dialog.setScene(dialogScene);
     dialog.show();
 
