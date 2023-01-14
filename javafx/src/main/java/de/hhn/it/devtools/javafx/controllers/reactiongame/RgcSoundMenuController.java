057000306e2a2108bf395fc64739221e91b16272
Tobias Bergtold <tbergtold@stud.hs-heilbronn.de>
2023-01-14 17:27:40 +0100

    + Added RgcSoundMenu.fxml + Added RgcSoundMenuController.java

---
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/reactiongame/RgcSoundMenuController.java
@@ -0,0 +1,29 @@
+package de.hhn.it.devtools.javafx.controllers.reactiongame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Slider;
+
+  public class RgcSoundMenuController {
+
+    @FXML // fx:id="MasterSlider"
+    private Slider MasterSlider; // Value injected by FXMLLoader
+
+    @FXML // fx:id="SfxSlider"
+    private Slider SfxSlider; // Value injected by FXMLLoader
+
+    @FXML // fx:id="MusicSlider"
+    private Slider MusicSlider; // Value injected by FXMLLoader
+
+    @FXML // fx:id="MuteCheckbox"
+    private CheckBox MuteCheckbox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="MenuButton"
+    private Button MenuButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="SaveButton"
+    private Button SaveButton; // Value injected by FXMLLoader
+
+  }
+
