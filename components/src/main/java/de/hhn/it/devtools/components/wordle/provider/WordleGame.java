71583453cdef321e62d883b6402c32d50e759d7f
MDrautz <madrautz@stud.hs-heilbronn.de>
2023-01-15 11:11:37 +0100

    fix: deleted superfluous methods in WordleGame

--- components/src/main/java/de/hhn/it/devtools/components/wordle/provider/WordleGame.java
+++ components/src/main/java/de/hhn/it/devtools/components/wordle/provider/WordleGame.java
@@ -4,41 +4,27 @@ import de.hhn.it.devtools.apis.wordle.IllegalGuessException;
 
 public class WordleGame {
 
-  private String solution;
   private final WordleGuess[] playerGuesses;
   private int wordleGuessIndex;
-  private int wordlePanelIndex;
-  private final WordleGameLogic backend;
-  private boolean isGameFinished;
 
-  public WordleGame(String solution, WordleGameLogic backend) {
-    this.solution = solution;
+  public WordleGame() {
     this.playerGuesses = new WordleGuess[6];
     this.wordleGuessIndex = 0;
-    this.wordlePanelIndex = 0;
-    this.backend = backend;
     for (int i = 0; i <= 5; i++){
       playerGuesses[i] = new WordleGuess();
     }
-  }
-
-  public String getSolution() {
-    return solution;
   }
 
   public WordleGuess[] getPlayerGuesses() {
     return playerGuesses;
   }
 
-  public WordleGameLogic getBackend() {
-    return backend;
-  }
-
   public int getWordleGuessIndex() {
     return wordleGuessIndex;
   }
 
-  public int getWordlePanelIndex() {
-    return wordlePanelIndex;
+  public void incrementWordleGuessIndex() {
+    wordleGuessIndex++;
   }
+
 }
