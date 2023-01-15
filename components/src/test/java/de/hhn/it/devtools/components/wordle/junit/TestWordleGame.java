3287d432cd977e4f18417f49a2a15c8388f8e84a
MDrautz <madrautz@stud.hs-heilbronn.de>
2023-01-15 11:35:10 +0100

    fix: added before each to initialize testGame and added test for incrementing the WordleGuessIndex

--- components/src/test/java/de/hhn/it/devtools/components/wordle/junit/TestWordleGame.java
+++ components/src/test/java/de/hhn/it/devtools/components/wordle/junit/TestWordleGame.java
@@ -1,22 +1,30 @@
 package de.hhn.it.devtools.components.wordle.junit;
 
-import de.hhn.it.devtools.apis.wordle.IllegalGuessException;
 import de.hhn.it.devtools.components.wordle.provider.WordleGame;
-import de.hhn.it.devtools.components.wordle.provider.WordleGameLogic;
-import de.hhn.it.devtools.components.wordle.provider.WordleGuess;
-import de.hhn.it.devtools.components.wordle.provider.WordlePanel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
 
 public class TestWordleGame {
 
+  WordleGame testGame;
+
+  @BeforeEach
+  void init(){
+    testGame = new WordleGame();
+  }
+
   @Test
   void testWordleGameConstructor() {
-    WordleGameLogic testLogic = new WordleGameLogic();
-    WordleGame testGame = new WordleGame();
     assertNotNull(testGame.getPlayerGuesses());
   }
+
+  @Test
+  void testIncrementGuessIndex() {
+    testGame.incrementWordleGuessIndex();
+    assertEquals(1,testGame.getWordleGuessIndex());
+  }
+
 }
