52f9e4aca9fde9e3f69e2842f577d563886ce0fa
Lukas Fahrner <lfahrner@stud.hs-heilbronn.de>
2023-01-18 21:07:39 +0100

    Added HashMap for descriptions and created binds for Stats

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/CharacterSheetController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/CharacterSheetController.java
@@ -1,18 +1,21 @@
 package de.hhn.it.devtools.javafx.controllers;
 
 import de.hhn.it.devtools.apis.ttrpgsheets.CharacterDescriptor;
+import de.hhn.it.devtools.apis.ttrpgsheets.CharacterSheet;
 import de.hhn.it.devtools.apis.ttrpgsheets.CharacterSheetListener;
 import de.hhn.it.devtools.apis.ttrpgsheets.DescriptionDescriptor;
+import de.hhn.it.devtools.apis.ttrpgsheets.DescriptionType;
 import de.hhn.it.devtools.apis.ttrpgsheets.DiceDescriptor;
+import de.hhn.it.devtools.apis.ttrpgsheets.DiceType;
 import de.hhn.it.devtools.apis.ttrpgsheets.OriginType;
 import de.hhn.it.devtools.apis.ttrpgsheets.StatDescriptor;
 import de.hhn.it.devtools.apis.ttrpgsheets.StatType;
 import de.hhn.it.devtools.components.ttrpgsheets.DefaultCharacterSheet;
 import java.util.HashMap;
-import java.util.Map;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -88,31 +91,65 @@ public class CharacterSheetController extends Controller implements CharacterShe
   @FXML
   private TextField weightTextField;
 
-  private final DefaultCharacterSheet characterSheet;
+  private final CharacterSheet characterSheet;
 
   HashMap<StatType, SimpleIntegerProperty> stat2Property;
-
-  private final SimpleBooleanProperty isLevelOne;
-  // TODO Properties erstellen für alle Labels usw.
+  HashMap<DescriptionType, SimpleStringProperty> description2Property;
+  SimpleIntegerProperty diceProperty;
 
   public CharacterSheetController() {
     characterSheet = new DefaultCharacterSheet(CharacterDescriptor.EMPTY);
-    isLevelOne = new SimpleBooleanProperty();
     stat2Property = new HashMap<>();
+    description2Property = new HashMap<>();
     for (StatType statType : StatType.values()) {
       stat2Property.put(statType, new SimpleIntegerProperty());
     }
-    // TODO vlt. ein standard ausgefüllter Bogen am anfang laden
+    for (DescriptionType descriptionType : DescriptionType.values()) {
+      description2Property.put(descriptionType, new SimpleStringProperty());
+    }
+    diceProperty = new SimpleIntegerProperty();
   }
 
   @FXML
   void initialize() {
     characterSheet.addCallback(this);
-    // TODO Properties hier binden
+    // Stats binding
+    // Level
     levelLabel.textProperty().bind(Bindings.createStringBinding(
             () -> String.valueOf(stat2Property.get(StatType.LEVEL).get()),
             stat2Property.get(StatType.LEVEL)));
+    // Health / Max health
+    healthLabel.textProperty().bind(Bindings.createStringBinding(
+            () -> String.format("%d/%d", stat2Property.get(StatType.HEALTH).get(),
+            stat2Property.get(StatType.MAX_HEALTH).get()),
+            stat2Property.get(StatType.HEALTH), stat2Property.get(StatType.MAX_HEALTH)));
+    // Defence
+    defenceLabel.textProperty().bind(Bindings.createStringBinding(
+            () -> String.valueOf(stat2Property.get(StatType.DEFENCE).get()),
+            stat2Property.get(StatType.DEFENCE)));
+    // Strength
+    strengthLabel.textProperty().bind(Bindings.createStringBinding(
+            () -> String.valueOf(stat2Property.get(StatType.STRENGTH).get()),
+            stat2Property.get(StatType.STRENGTH)));
+    // Agility
+    agilityLabel.textProperty().bind(Bindings.createStringBinding(
+            () -> String.valueOf(stat2Property.get(StatType.AGILITY).get()),
+            stat2Property.get(StatType.AGILITY)));
+    // Dexterity
+    dexterityLabel.textProperty().bind(Bindings.createStringBinding(
+            () -> String.valueOf(stat2Property.get(StatType.DEXTERITY).get()),
+            stat2Property.get(StatType.DEXTERITY)));
 
+    // Descriptions binding
+    // Player name
+    playerNameTextField.textProperty().addListener((o, oldValue, newValue) ->
+            characterSheet.changeDescription(DescriptionType.PLAYER_NAME, newValue));
+    playerNameTextField.textProperty().bindBidirectional(
+            description2Property.get(DescriptionType.PLAYER_NAME));
+
+    // Dice binding
+    diceLabel.textProperty().bind(Bindings.createStringBinding(
+            () -> String.valueOf(diceProperty.get()), diceProperty));
   }
 
   @FXML
@@ -222,12 +259,52 @@ public class CharacterSheetController extends Controller implements CharacterShe
 
   @FXML
   void onRollDice(ActionEvent event) {
-
+    characterSheet.rollDice();
   }
 
   @FXML
   void onSave(ActionEvent event) {
 
+  }
+
+  @FXML
+  void onSetD2(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D2);
+  }
+
+  @FXML
+  void onSetD4(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D4);
+  }
+
+  @FXML
+  void onSetD6(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D6);
+  }
+
+  @FXML
+  void onSetD8(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D8);
+  }
+
+  @FXML
+  void onSetD10(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D10);
+  }
+
+  @FXML
+  void onSetD12(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D12);
+  }
+
+  @FXML
+  void onSetD20(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D20);
+  }
+
+  @FXML
+  void onSetD100(ActionEvent event) {
+    characterSheet.changeDiceType(DiceType.D100);
   }
 
   @FXML
@@ -250,20 +327,36 @@ public class CharacterSheetController extends Controller implements CharacterShe
 
   }
 
+  // Callbacks
   @Override
   public void statChanged(StatDescriptor stat) {
-    StatType statType = stat.getStatType();
-    stat2Property.get(statType).set(characterSheet.getStatDisplayValue(statType));
+    Platform.runLater(() -> {
+      StatType statType = stat.getStatType();
+      stat2Property.get(statType).set(characterSheet.getStatDisplayValue(statType));
+    });
   }
 
   @Override
   public void descriptionChanged(DescriptionDescriptor description) {
-
+    Platform.runLater(() ->
+            description2Property.get(description.getDescriptionType()).set(description.getText()));
   }
 
   @Override
   public void diceChanged(DiceDescriptor dice) {
-
+    Platform.runLater(() -> {
+      diceProperty.set(dice.getResult());
+      switch (dice.getDiceType()) {
+        case D2 -> diceMenuButton.setText("D2");
+        case D4 -> diceMenuButton.setText("D4");
+        case D6 -> diceMenuButton.setText("D6");
+        case D8 -> diceMenuButton.setText("D8");
+        case D10 -> diceMenuButton.setText("D10");
+        case D12 -> diceMenuButton.setText("D12");
+        case D20 -> diceMenuButton.setText("D20");
+        case D100 -> diceMenuButton.setText("D100");
+      }
+    });
   }
 
   @Override
