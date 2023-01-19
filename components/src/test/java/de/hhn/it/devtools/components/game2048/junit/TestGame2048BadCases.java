fee15db4aca042cefa2bbd95b72876ac6034b5cf
Dominik Buzov <dominikbuzov1@gmail.com>
2023-01-19 17:09:19 +0100

    Added file for bad test cases.

---
+++ components/src/test/java/de/hhn/it/devtools/components/game2048/junit/TestGame2048BadCases.java
@@ -0,0 +1,8 @@
+package de.hhn.it.devtools.components.game2048.junit;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Test Game2048 and Game2048Service with bad cases. ")
+public class TestGame2048BadCases {
+
+}
