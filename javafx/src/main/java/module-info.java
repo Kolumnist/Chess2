0228f254b33c7692df2779882ef0dd0e1bbd3fb1
Patrick Franz <patrick.franz@franz-vs.de>
2023-01-20 15:58:48 +0100

    feat: added necessary statements to module-info.java

--- javafx/src/main/java/module-info.java
+++ javafx/src/main/java/module-info.java
@@ -6,9 +6,12 @@ module devtools.javafx {
   requires javafx.fxml;
   uses de.hhn.it.devtools.apis.examples.coffeemakerservice.CoffeeMakerService;
   uses de.hhn.it.devtools.apis.examples.coffeemakerservice.AdminCoffeeMakerService;
+  uses de.hhn.it.devtools.apis.wordle.WordleService;
+  opens de.hhn.it.devtools.javafx.controllers.wordleAdditional to javafx.fxml;
   opens de.hhn.it.devtools.javafx.controllers to javafx.fxml;
   opens de.hhn.it.devtools.javafx.controllers.coffeemaker to javafx.fxml;
   opens de.hhn.it.devtools.javafx.controllers.template to javafx.fxml;
+  exports de.hhn.it.devtools.javafx.controllers.wordleAdditional;
   exports de.hhn.it.devtools.javafx;
   exports de.hhn.it.devtools.javafx.controllers;
   exports de.hhn.it.devtools.javafx.controllers.coffeemaker;
