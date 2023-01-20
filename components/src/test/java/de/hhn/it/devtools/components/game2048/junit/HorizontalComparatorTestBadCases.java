eb54bb6df57d5d0b79954dc981f0c92410ddbeb0
Emanuel Buzov <emanuelbuzov@gmail.com>
2023-01-20 22:37:23 +0100

    fixed and cleaned code

--- components/src/test/java/de/hhn/it/devtools/components/game2048/junit/HorizontalComparatorTestBadCases.java
+++
@@ -1,8 +0,0 @@
-package de.hhn.it.devtools.components.game2048.junit;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("Test für schlechte Beispiele des HorizontalComparator. ")
-public class HorizontalComparatorTestBadCases {
-
-}
