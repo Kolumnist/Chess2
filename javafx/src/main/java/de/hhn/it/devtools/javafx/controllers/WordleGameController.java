2fdbe361387d3c4f804be1fbe604a80ed6920e61
Patrick Franz <patrick.franz@franz-vs.de>
2023-01-20 18:15:26 +0100

    fix: moved skeletion from WordleGame.fxml from WordlePanelController to WordleGameController

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/WordleGameController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/WordleGameController.java
@@ -14,7 +14,95 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class WordleGameController extends Controller implements Initializable {
+    @FXML
+    private Label fifthRowFirstLabel;
 
+    @FXML
+    private Label firstRowFourthLabel;
+
+    @FXML
+    private Label firstRowFifthLabel;
+
+    @FXML
+    private Label fifthRowFifthLabel;
+
+    @FXML
+    private Label fifthRowSecondLabel;
+
+    @FXML
+    private Label firstRowFirstLabel;
+
+    @FXML
+    private Label sixthRowFourthLabel;
+
+    @FXML
+    private Label thirdRowFirstLabel;
+
+    @FXML
+    private Label thirdRowSecondLabel;
+
+    @FXML
+    private Label secondRowSecondLabel;
+
+    @FXML
+    private Label thirdRowFifthLabel;
+
+    @FXML
+    private Label sixthRowSecondLabel;
+
+    @FXML
+    private Label fourthRowFirstLabel;
+
+    @FXML
+    private Label firstRowFSecondLabel;
+
+    @FXML
+    private Label fourthRowThirdLabel;
+
+    @FXML
+    private Label thirdRowFourthLabel;
+
+    @FXML
+    private Label fourthRowFourthLabel;
+
+    @FXML
+    private Label fifthRowThirdLabel;
+
+    @FXML
+    private Label fourthRowSecondLabel;
+
+    @FXML
+    private Label firstRowThirdLabel;
+
+    @FXML
+    private Label sixthRowThirdLabel;
+
+    @FXML
+    private Label secondRowFirstLabel;
+
+    @FXML
+    private Label secondRowFifthLabel;
+
+    @FXML
+    private Label fourthRowFifthLabel;
+
+    @FXML
+    private Label fifthRowFourthLabel;
+
+    @FXML
+    private Label secondRowFourthLabel;
+
+    @FXML
+    private Label sixthRowFirstLabel;
+
+    @FXML
+    private Label secondRowThirdLabel;
+
+    @FXML
+    private Label sixthRowFifthLabel;
+
+    @FXML
+    private Label thirdRowThirdLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
