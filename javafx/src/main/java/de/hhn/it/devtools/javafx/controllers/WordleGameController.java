194b2109e7569fd289cc4fcefe4fcff58fc2057f
Patrick Franz <patrick.franz@franz-vs.de>
2023-01-20 16:08:50 +0100

    feat: added new WordleGameController class

---
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/WordleGameController.java
@@ -0,0 +1,23 @@
+package de.hhn.it.devtools.javafx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import de.hhn.it.devtools.apis.wordle.WordleService;
+import de.hhn.it.devtools.apis.wordle.WordleGuessService;
+import de.hhn.it.devtools.apis.wordle.IllegalGuessException;
+import de.hhn.it.devtools.apis.wordle.WordlePanelService;
+import de.hhn.it.devtools.apis.wordle.WordlePanelListener;
+import de.hhn.it.devtools.javafx.controllers.WordleGameController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WordleGameController extends Controller implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
