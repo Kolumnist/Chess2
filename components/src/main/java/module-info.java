10e0a71a0c3156fcd15300460cb8fd9cee51a25a
MDrautz <madrautz@stud.hs-heilbronn.de>
2023-01-20 14:16:12 +0100

    feat: added exports and provides with statement to module-info.java

--- components/src/main/java/module-info.java
+++ components/src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module devtools.components {
   exports de.hhn.it.devtools.components.example.coffeemakerservice.provider;
+  exports de.hhn.it.devtools.components.wordle.provider;
   requires org.slf4j;
   requires devtools.apis;
   provides  de.hhn.it.devtools.apis.examples.coffeemakerservice.CoffeeMakerService
           with de.hhn.it.devtools.components.example.coffeemakerservice.provider.WnckCoffeeMakerService;
   provides  de.hhn.it.devtools.apis.examples.coffeemakerservice.AdminCoffeeMakerService
           with de.hhn.it.devtools.components.example.coffeemakerservice.provider.WnckCoffeeMakerService;
+  provides de.hhn.it.devtools.apis.wordle.WordleService
+          with de.hhn.it.devtools.components.wordle.provider.WordleGameLogic;
         }
