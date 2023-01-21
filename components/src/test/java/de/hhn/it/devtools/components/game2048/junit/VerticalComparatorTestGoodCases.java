7fcfaa796fed8ce390b955dadf23209b12ec3268
Dominik Buzov <dominikbuzov1@gmail.com>
2023-01-21 21:03:56 +0100

    Improved testing for HorizontalComparatorTestGoodCases and VerticalComparatorTestGoodCases

--- components/src/test/java/de/hhn/it/devtools/components/game2048/junit/VerticalComparatorTestGoodCases.java
+++ components/src/test/java/de/hhn/it/devtools/components/game2048/junit/VerticalComparatorTestGoodCases.java
@@ -1,7 +1,5 @@
 package de.hhn.it.devtools.components.game2048.junit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
 import de.hhn.it.devtools.apis.game2048.Block;
 import de.hhn.it.devtools.apis.game2048.MovingDirection;
@@ -10,12 +8,14 @@ import de.hhn.it.devtools.components.game2048.provider.Comparators.VerticalCompa
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("Test for good cases of the VerticalComparator. ")
 public class VerticalComparatorTestGoodCases {
 
   @Test
   @DisplayName("Test if vertical comparator compares the different blocks correctly" +
-  " for up. ")
+      " for up. ")
   public void testCompareUp() {
     VerticalComparator comparator = null;
     try {
@@ -28,6 +28,7 @@ public class VerticalComparatorTestGoodCases {
     Block block2 = new Block(new Position(1,2), 2);
     Block block3 = new Block(new Position(1,3), 4);
 
+    assert comparator != null;
     assertEquals(1, comparator.compare(block1, block2));
     assertEquals(0, comparator.compare(block2, block2));
     assertEquals(-1, comparator.compare(block3, block2));
@@ -35,7 +36,7 @@ public class VerticalComparatorTestGoodCases {
 
   @Test
   @DisplayName("Test if vertical comparator compares the different blocks correctly" +
-  "for down. ")
+      "for down. ")
   public void testCompareDown() {
     VerticalComparator comparator = null;
     try {
@@ -48,6 +49,7 @@ public class VerticalComparatorTestGoodCases {
     Block block2 = new Block(new Position(1,2), 2);
     Block block3 = new Block(new Position(1,3), 4);
 
+    assert comparator != null;
     assertEquals(-1, comparator.compare(block1, block2));
     assertEquals(0, comparator.compare(block2, block2));
     assertEquals(1, comparator.compare(block3, block2));
