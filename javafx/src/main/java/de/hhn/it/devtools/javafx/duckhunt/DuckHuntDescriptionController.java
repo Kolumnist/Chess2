e3f6c39303e85b1216030fd5a8013b0bd57f06d4
Jannik Döring <jdoering@stud.hs-heilbronn.de>
2023-01-21 15:29:44 +0100

    fixed DuckHuntDescriptionController and added a back button

--- javafx/src/main/java/de/hhn/it/devtools/javafx/duckhunt/DuckHuntDescriptionController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/duckhunt/DuckHuntDescriptionController.java
@@ -1,10 +1,16 @@
 package de.hhn.it.devtools.javafx.duckhunt;
 
-import javafx.fxml.Initializable;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
+/**
+ * Controller for the Description (how to play) screen.
+ */
 public class DuckHuntDescriptionController implements Initializable {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(DuckHuntDescriptionController.class);
@@ -12,11 +18,29 @@ public class DuckHuntDescriptionController implements Initializable {
   public static final String SCREEN = "description.screen";
   private DuckHuntScreenController screenController;
 
+  @FXML
+  private ResourceBundle resources;
+
+  @FXML
+  private URL location;
+
+  @FXML
+  private Button backButton;
+
+  @FXML
+  private AnchorPane templateAnchorPane;
+
+  @FXML
+  void backToMainMenu(MouseEvent event) {
+    screenController.switchTo(SCREEN, DuckHuntMenuController.SCREEN);
+  }
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     DuckHuntAttributeStore duckHuntAttributeStore = DuckHuntAttributeStore.getReference();
     screenController =
         (DuckHuntScreenController) duckHuntAttributeStore
             .getAttribute(DuckHuntScreenController.SCREEN_CONTROLLER);
+    logger.info("Description Screen initialized.");
   }
 }
