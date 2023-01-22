592cb393096b5f8080fd414b8c1102b89f30bfae
MDrautz <madrautz@stud.hs-heilbronn.de>
2023-01-22 17:42:20 +0100

    style: reindented WordleGameController with 2 spaces

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/WordleGameController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/WordleGameController.java
@@ -19,7 +19,6 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
@@ -28,152 +27,152 @@ import java.util.ResourceBundle;
 public class WordleGameController extends Controller implements Initializable {
 
 
-    @FXML
-    private TextField textField;
-    @FXML
-    private GridPane rowGridPane;
+  @FXML
+  private TextField textField;
+  @FXML
+  private GridPane rowGridPane;
 
-    @FXML
-    private Button button;
+  @FXML
+  private Button button;
 
-    @FXML
-    private Button playAnotherRound;
+  @FXML
+  private Button playAnotherRound;
 
-    private Integer rowCount;
+  private Integer rowCount;
 
-    private Integer colCount = 0;
+  private Integer colCount = 0;
 
-    private String userInput;
+  private String userInput;
 
-    private final WordleService backend = new WordleGameLogic();
+  private final WordleService backend = new WordleGameLogic();
 
-    private boolean isGameRunning = true;
-
-
-    @FXML
-    void enterButtonClicked(ActionEvent event) throws IllegalGuessException, IOException {
-
-        if(checkIfGuessWasValid()) {
-            isGameRunning = !backend.receiveAndComputeGuess(textField.getText());
-            fillCurrentRowWithUserInput();
-            textField.clear();
-        }
-        if (!isGameRunning){
-            textField.setEditable(false);
-            button.setDisable(true);
-            System.out.println("eZ win");
-            playAnotherRound.setVisible(true);
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Victory");
-            alert.setHeaderText("Congratulations! You won!");
-            alert.show();
+  private boolean isGameRunning = true;
 
 
-        }
-        if (isGameRunning && rowCount > 5){
-            textField.setEditable(false);
-            button.setDisable(true);
-            System.out.println("-35 LP");
-            playAnotherRound.setVisible(true);
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Defeat");
-            alert.setHeaderText("Oh no! You lost!");
-            alert.setContentText("Better luck next time!");
-            alert.show();
-        }
+  @FXML
+  void enterButtonClicked(ActionEvent event) throws IllegalGuessException, IOException {
+
+    if(checkIfGuessWasValid()) {
+      isGameRunning = !backend.receiveAndComputeGuess(textField.getText());
+      fillCurrentRowWithUserInput();
+      textField.clear();
+    }
+    if (!isGameRunning){
+      textField.setEditable(false);
+      button.setDisable(true);
+      System.out.println("eZ win");
+      playAnotherRound.setVisible(true);
+      Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setTitle("Victory");
+      alert.setHeaderText("Congratulations! You won!");
+      alert.show();
+
+
+    }
+    if (isGameRunning && rowCount > 5){
+      textField.setEditable(false);
+      button.setDisable(true);
+      System.out.println("-35 LP");
+      playAnotherRound.setVisible(true);
+      Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setTitle("Defeat");
+      alert.setHeaderText("Oh no! You lost!");
+      alert.setContentText("Better luck next time!");
+      alert.show();
+    }
+  }
+
+  @FXML
+  void restartGame(ActionEvent event) {
+    backend.startAnotherGame();
+    isGameRunning = true;
+    for(Node node: rowGridPane.getChildren()) {
+      Label currentLabel = (Label) node;
+      currentLabel.setBackground(new Background(new BackgroundFill(Color.rgb(18,18,19), CornerRadii.EMPTY, Insets.EMPTY)));
+      currentLabel.setText(" ");
+    }
+    textField.setEditable(true);
+    button.setDisable(false);
+    rowCount = null;
+    try {
+      connectListenersToLabels();
+    } catch (IllegalParameterException e) {
+      throw new RuntimeException(e);
+    }
+    playAnotherRound.setVisible(false);
+  }
+
+  private boolean checkIfGuessWasValid() {
+    userInput = textField.getText();
+
+    char[] inputArray = userInput.toCharArray();
+
+    if(userInput.length() < 5) {
+      Alert alert = new Alert(Alert.AlertType.ERROR);
+      alert.setTitle("Error");
+      alert.setHeaderText("Error!");
+      alert.setContentText("The guess was not long enough! Try again!");
+      alert.show();
+      return false;
     }
 
-    @FXML
-    void restartGame(ActionEvent event) {
-        backend.startAnotherGame();
-        isGameRunning = true;
-        for(Node node: rowGridPane.getChildren()) {
-            Label currentLabel = (Label) node;
-            currentLabel.setBackground(new Background(new BackgroundFill(Color.rgb(18,18,19), CornerRadii.EMPTY, Insets.EMPTY)));
-            currentLabel.setText(" ");
-            }
-        textField.setEditable(true);
-        button.setDisable(false);
-        rowCount = null;
-        try {
-            connectListenersToLabels();
-        } catch (IllegalParameterException e) {
-            throw new RuntimeException(e);
-        }
-        playAnotherRound.setVisible(false);
-        }
-
-    private boolean checkIfGuessWasValid() {
-        userInput = textField.getText();
-
-        char[] inputArray = userInput.toCharArray();
-
-        if(userInput.length() < 5) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Error!");
-            alert.setContentText("The guess was not long enough! Try again!");
-            alert.show();
-            return false;
-        }
-
-        if(userInput.length() > 5) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Error!");
-            alert.setContentText("The guess was too long! Try again!");
-            alert.show();
-            return false;
-        }
-
-        for(int i = 0; i < 5; i++) {
-            if (!Character.isLetter(inputArray[i])) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Error!");
-                alert.setContentText("Only letters are allowed!");
-                alert.show();
-                return false;
-            }
-        }
-        return true;
+    if(userInput.length() > 5) {
+      Alert alert = new Alert(Alert.AlertType.ERROR);
+      alert.setTitle("Error");
+      alert.setHeaderText("Error!");
+      alert.setContentText("The guess was too long! Try again!");
+      alert.show();
+      return false;
     }
 
-    private void fillCurrentRowWithUserInput() {
-        userInput = textField.getText();
-
-        for(Node node: rowGridPane.getChildren()) {
-            if(Objects.equals(GridPane.getRowIndex(node), rowCount)) {
-                Label currentLabel = (Label) node;
-                currentLabel.setText(userInput.substring(colCount, colCount + 1).toUpperCase());
-                colCount++;
-            }
-        }
-        if(rowCount == null) {
-            rowCount = 0;
-        }
-        rowCount = rowCount +1;
-        colCount = 0;
+    for(int i = 0; i < 5; i++) {
+      if (!Character.isLetter(inputArray[i])) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Error!");
+        alert.setContentText("Only letters are allowed!");
+        alert.show();
+        return false;
+      }
     }
+    return true;
+  }
 
-    private void connectListenersToLabels() throws IllegalParameterException {
-        int counter = 0;
-        for (Node node: rowGridPane.getChildren()) {
-            Label currentLabel = (Label) node;
-            backend.addCallback(new SimpleWordlePanelListener(currentLabel), backend.getPanelById(counter));
-            counter++;
-        }
-    }
+  private void fillCurrentRowWithUserInput() {
+    userInput = textField.getText();
 
-    @Override
-        public void initialize (URL url, ResourceBundle resourceBundle){
-        playAnotherRound.setVisible(false);
-        backend.startGame();
-            try {
-                connectListenersToLabels();
-            } catch (IllegalParameterException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    for(Node node: rowGridPane.getChildren()) {
+      if(Objects.equals(GridPane.getRowIndex(node), rowCount)) {
+        Label currentLabel = (Label) node;
+        currentLabel.setText(userInput.substring(colCount, colCount + 1).toUpperCase());
+        colCount++;
+      }
     }
+    if(rowCount == null) {
+      rowCount = 0;
+    }
+    rowCount = rowCount +1;
+    colCount = 0;
+  }
+
+  private void connectListenersToLabels() throws IllegalParameterException {
+    int counter = 0;
+    for (Node node: rowGridPane.getChildren()) {
+      Label currentLabel = (Label) node;
+      backend.addCallback(new SimpleWordlePanelListener(currentLabel), backend.getPanelById(counter));
+      counter++;
+    }
+  }
+
+  @Override
+  public void initialize (URL url, ResourceBundle resourceBundle){
+    playAnotherRound.setVisible(false);
+    backend.startGame();
+    try {
+      connectListenersToLabels();
+    } catch (IllegalParameterException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
 
