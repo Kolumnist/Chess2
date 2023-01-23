2d2f7159314e367a470970e8719745b2fa6a15fa
shayarae <shira.schaefer836@yahoo.de>
2023-01-23 22:48:30 +0100

    added comment for overview

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/MemoryServiceController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/MemoryServiceController.java
@@ -81,9 +81,12 @@ public class MemoryServiceController extends Controller implements Initializable
    * @return CardSetDescriptor of the chosen difficulty
    */
   public CardSetDescriptor fetchCardSets(String difficulty) {
+    //funktioniert nicht
     //File folder = new File(Objects.requireNonNull(getClass().getClassLoader()
     //  .getResource("/fxml/memory/pictures/sets/" + difficulty + "/")).getFile());
+    //funktioniert für Aufruf der main
     //File folder = new File("javafx/src/main/resources/fxml/memory/pictures/sets/" + difficulty);
+    //funktioniert für gradle run
     File folder = new File(getClass().getResource("/fxml/memory/pictures/sets/"+difficulty).getFile());
     HashMap<Integer, String> pictureReferences = new HashMap<>();
     PictureCardDescriptor[] pictureCardDescriptors = new PictureCardDescriptor[5 * 4];
